53e730ca64cba31018ebb251c7ce37c9e3432f67
Matrostan <matrosovdream@gmail.com>
2025-03-05 09:59:12 +0100

    Admin service editor, changed layout of custom fields

--- resources/views/dashboard/services/modals/edit-form-field.blade.php
+++ resources/views/dashboard/services/modals/edit-form-field.blade.php
@@ -20,7 +20,9 @@
                     <input type="hidden" name="required" value="0" />
                     <input type="hidden" name="entity" value="general" />
 
-                    <div class="mb-10 w-50 fv-row fv-plugins-icon-container">
+                    <div class="row mb-5">
+
+                        <div class="col-lg-6 fv-row fv-plugins-icon-container">
                         <label class="required form-label">Field</label>
                         <select name="field_id" class="form-select form-select-solid mb-2">
                             <option>Select field</option>
@@ -28,49 +30,47 @@
                                 <option 
                                     value="{{ $field['id'] }}"
                                     {{ $fieldValue['field_id'] == $field['id'] ? 'selected' : '' }}
-                                    >{{ $field['title'] }}
+                                    >
+                                    {{ $field['title'] }}
                                 </option>
                             @endforeach
                         </select>
-                    </div>
+                        </div>
 
-                    <div class="mb-10 w-50 fv-row fv-plugins-icon-container">
+                        <div class="col-lg-6 fv-row fv-plugins-icon-container">
                         <label class="required form-label">Required</label>
                         <select name="required" class="form-select form-select-solid mb-2">
                             <option value="0" {{ $fieldValue['required'] == 0 ? 'selected' : '' }}>No</option>
                             <option value="1" {{ $fieldValue['required'] == 1 ? 'selected' : '' }}>Yes</option>
                         </select>
+                        </div>
+
                     </div>
 
-                    <div class="mb-10 w-50 fv-row fv-plugins-icon-container">
+                    <div class="row mb-5">
+
+                        <div class="col-lg-6 fv-row fv-plugins-icon-container">
                         <label class="form-label">Default value</label>
                         <input type="text" name="default_value" class="form-control form-control-solid mb-2" value="{{ $fieldValue['default_value'] ?? '' }}" />
-                    </div>
+                        </div>
 
-                    <div class="mb-10 w-50 fv-row fv-plugins-icon-container">
+                        <div class="col-lg-6 fv-row fv-plugins-icon-container">
                         <label class="form-label">Placeholder</label>
                         <input type="text" name="placeholder" class="form-control form-control-solid mb-2" value="{{ $fieldValue['placeholder'] ?? '' }}" />
+                        </div>
+
                     </div>
 
-                    <div class="mb-10 w-50 fv-row fv-plugins-icon-container">
+                    <div class="row mb-5">
+
+                        <div class="col-lg-6 fv-row fv-plugins-icon-container">
                         <label class="form-label">Classes</label>
                         <input type="text" name="classes" class="form-control form-control-solid mb-2" value="{{ $fieldValue['classes'] ?? '' }}" />
-                    </div>
+                        </div>
 
-                    <div class="mb-10 w-50 fv-row fv-plugins-icon-container">
+                        <div class="col-lg-6 fv-row fv-plugins-icon-container">
                         <label class="form-label">Order</label>
                         <input type="number" name="order" class="form-control form-control-solid mb-2" value="{{ $fieldValue['order'] ?? '' }}" />
-                    </div>
-
-                    <div class="d-flex flex-stack">
-
-                        <div>
-                            <button type="submit" class="btn btn-lg btn-primary">
-                                Save
-                                <i class="ki-duotone ki-arrow-right fs-3 ms-1 me-0">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </i></button>
                         </div>
 
                     </div>
@@ -79,15 +79,33 @@
 
                 <hr />
 
-                <form action="{{ route('dashboard.services.fields.destroy', ['service' => $service['id'], 'field_id' => $fieldValue['id']]) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
+                
 
-                    <button type="submit" class="btn btn-danger alert">
-                        Delete field
-                    </button>
 
-                </form>
+                <div class="row mb-5">
+
+                    <div class="col-lg-6 fv-row fv-plugins-icon-container">
+                        <form action="{{ route('dashboard.services.fields.destroy', ['service' => $service['id'], 'field_id' => $fieldValue['id']]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" class="btn btn-danger alert">
+                                Delete field
+                            </button>
+
+                        </form>
+                    </div>
+
+                    <div class="col-lg-6 fv-row fv-plugins-icon-container text-end">
+                    <button type="submit" class="btn btn-lg btn-primary">
+                                Save
+                                <i class="ki-duotone ki-arrow-right fs-3 ms-1 me-0">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i></button>
+                    </div>
+
+                </div>
 
             </div>
         </div>
