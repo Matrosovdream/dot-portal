89ca85d9fc50024b2a2f2a69e7bb71ab679db6ba
dotfiler <matrosovdream@gmail.com>
2025-03-08 22:26:51 +0100

    Basic Gateway interface

--- app/Http/Controllers/Dashboard/RequestAdminController.php
+++ app/Http/Controllers/Dashboard/RequestAdminController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Actions\Dashboard\RequestAdminActions;
-use Illuminate\Http\Request;
 
 class RequestAdminController extends Controller
 {
