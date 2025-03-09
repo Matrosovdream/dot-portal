c2dcc8fdff3ff13619e78acd7d1e86c647b858f5
dotfiler <matrosovdream@gmail.com>
2025-03-09 15:27:45 +0100

    User's dashboard, removed checkbox columns in all lists

--- resources/views/dashboard/servicerequest/history/index.blade.php
+++ resources/views/dashboard/servicerequest/history/index.blade.php
@@ -12,12 +12,6 @@
                 <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_products_table">
                     <thead>
                         <tr class="text-start text-gray-500 fw-bold fs-7 text-uppercase gs-0">
-                            <th class="w-10px pe-2">
-                                <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
-                                    <input class="form-check-input" type="checkbox" data-kt-check="true"
-                                        data-kt-check-target="#kt_ecommerce_products_table .form-check-input" value="1" />
-                                </div>
-                            </th>
                             <th class="min-w-200px">Service</th>
                             <th class="min-w-200px text-center">Status</th>
                             <th class="min-w-200px text-center">Added</th>
@@ -29,11 +23,6 @@
                         @foreach($requests['items'] as $request)
 
                             <tr>
-                                <td>
-                                    <div class="form-check form-check-sm form-check-custom form-check-solid">
-                                        <input class="form-check-input" type="checkbox" value="1" />
-                                    </div>
-                                </td>
                                 <td>
                                     <a href="{{ route('dashboard.servicerequest.history.show', $request['id']) }}"
                                         class="text-gray-800 text-hover-primary fs-5 fw-bold"
