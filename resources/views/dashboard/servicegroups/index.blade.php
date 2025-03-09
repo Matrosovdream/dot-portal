98e857f7e30ff265b36a716d22beb7a39631c97a
dotfiler <matrosovdream@gmail.com>
2025-03-09 15:41:07 +0100

    Admin's dashboard, removed checkbox columns in all lists

--- resources/views/dashboard/servicegroups/index.blade.php
+++ resources/views/dashboard/servicegroups/index.blade.php
@@ -30,12 +30,6 @@
             <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_products_table">
                 <thead>
                     <tr class="text-start text-gray-500 fw-bold fs-7 text-uppercase gs-0">
-                        <th class="w-10px pe-2">
-                            <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
-                                <input class="form-check-input" type="checkbox" data-kt-check="true"
-                                    data-kt-check-target="#kt_ecommerce_products_table .form-check-input" value="1" />
-                            </div>
-                        </th>
                         <th class="min-w-200px">Name</th>
                         <th class="min-w-200px text-center">Slug</th>
                         <th class="min-w-200px text-center">Description</th>
@@ -48,11 +42,6 @@
                     @foreach($groups['items'] as $group)
 
                         <tr>
-                            <td>
-                                <div class="form-check form-check-sm form-check-custom form-check-solid">
-                                    <input class="form-check-input" type="checkbox" value="1" />
-                                </div>
-                            </td>
                             <td>
                                 <a href="{{ route('dashboard.servicegroups.show', $group['id']) }}"
                                     class="text-gray-800 text-hover-primary fs-5 fw-bold"
