56faa03ad9584a4512a56a4ee931ac0a90932c69
dotfiler <matrosovdream@gmail.com>
2025-03-10 10:05:28 +0100

    User Payment Cards seeder, a bit cleaner user migrations

---
+++ database/seeders/UserPaymentCardSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\UserPaymentCard;
+
+class UserPaymentCardSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        $subs = [
+            ['user_id' => 1, 'card_number' => '411112254532', 'card_holder_name' => 'Stan Matrosov', 'expiry_date' => '2025-01-01'],
+            ['user_id' => 2, 'card_number' => '411212259855', 'card_holder_name' => 'Stan Matrosov', 'expiry_date' => '2027-03-01'],
+        ];
+        
+        foreach ($subs as $item) {
+            UserPaymentCard::firstOrCreate($item);
+        }
+
+    }
+}
