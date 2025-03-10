f996b245c1ad3f0b1b9d3aaa912bf64e8eb92b7f
dotfiler <matrosovdream@gmail.com>
2025-03-10 10:17:08 +0100

    User Payment Cards repo, this repo is connected to User Repo

---
+++ app/Repositories/User/UserPaymentCardRepo.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Repositories\User;
+
+use App\Repositories\AbstractRepo;
+use App\Models\UserPaymentCard;
+use App\Repositories\References\RefCountryStateRepo;
+
+
+
+class UserPaymentCardRepo extends AbstractRepo
+{
+
+    protected $countryStateRepo;
+
+    protected $model;
+
+    protected $fields = [];
+
+    protected $withRelations = [];
+
+    public function __construct()
+    {
+        $this->model = new UserPaymentCard();
+    }
+
+    public function mapItem($item)
+    {
+
+        if( empty($item) ) {
+            return null;
+        }
+
+        $res = [
+            'id' => $item->id,
+            'user_id' => $item->user_id,
+            'card_number' => $item->card_number,
+            'cardholder_name' => $item->card_holder_name,
+            'expiry_date' => $item->expiry_date,
+            'payment_method_id' => $item->payment_method_id,
+            'Model' => $item
+        ];
+
+        return $res;
+    }
+
+}
