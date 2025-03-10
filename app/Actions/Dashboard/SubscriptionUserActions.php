cfad0517ce78658088dedac6916b3a63a9511660
dotfiler <matrosovdream@gmail.com>
2025-03-10 12:31:26 +0100

    User payment history, repository, user actions binding

--- app/Actions/Dashboard/SubscriptionUserActions.php
+++ app/Actions/Dashboard/SubscriptionUserActions.php
@@ -4,27 +4,35 @@ namespace App\Actions\Dashboard;
 use App\Helpers\adminSettingsHelper;
 use App\Repositories\User\UserRepo;
 use App\Repositories\User\UserSubscriptionRepo;
+use App\Repositories\User\UserPaymentHistoryRepo;
 
 class SubscriptionUserActions {
 
     private $userRepo;
     private $userSubRepo;
+    private $userPaymentHistoryRepo;
 
     public function __construct()
     {
         $this->userRepo = new UserRepo();
         $this->userSubRepo = new UserSubscriptionRepo();
+        $this->userPaymentHistoryRepo = new UserPaymentHistoryRepo();
     }
 
     public function index()
     {
 
+        $user_id = auth()->user()->id;
+
         $data = [
             'title' => 'My subscription',
-            'user' => $this->userRepo->getByID( auth()->user()->id ),
-            'subscription' => $this->userSubRepo->getByUserID( auth()->user()->id ),
+            'user' => $this->userRepo->getByID( $user_id ),
+            'subscription' => $this->userSubRepo->getByUserID( $user_id ),
+            'paymentHistory' => $this->userPaymentHistoryRepo->getByUserID( $user_id ),
             'sidebarMenu' => adminSettingsHelper::getSidebarMenu(),
         ];
+
+        dd($data);
 
         return $data;
     }
