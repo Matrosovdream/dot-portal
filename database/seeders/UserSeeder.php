8a1c4c3fc50763aaa4e05514e826d0aadf6170b5
dotfiler <matrosovdream@gmail.com>
2025-03-26 13:01:37 +0100

    UserSeeder, added new user Driver and logic to updateOrCreate()

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,14 +14,18 @@ class UserSeeder extends Seeder
         $users = [
             ['firstname' => 'Admin', 'email' => 'admin@gmail.com', 'password' => '123456', 'role' => 1],
             ['firstname' => 'Manager', 'email' => 'manager@gmail.com', 'password' => '123456', 'role' => 2],
-            ['firstname' => 'user', 'email' => 'user@gmail.com', 'password' => '123456', 'role' => 3],
+            ['firstname' => 'Company', 'email' => 'company@gmail.com', 'password' => '123456', 'role' => 3],
+            ['firstname' => 'Driver', 'email' => 'driver@gmail.com', 'password' => '123456', 'role' => 4],
         ];
 
         foreach ($users as $userData) {
 
-            $user = User::firstOrCreate(
-                ['email' => $userData['email']], // Check by email
-                ['firstname' => $userData['firstname'], 'password' => Hash::make($userData['password'])] // Create if not found
+            $user = User::updateOrCreate(
+                ['email' => $userData['email']],
+                [
+                    'firstname' => $userData['firstname'], 
+                    'password' => Hash::make($userData['password'])
+                    ] 
             );
 
             // It fixes a bug
