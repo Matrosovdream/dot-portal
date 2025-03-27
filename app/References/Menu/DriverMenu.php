eaac0fbd819f410e659292d2f767984ca2eec426
dotfiler <matrosovdream@gmail.com>
2025-03-27 10:13:57 +0100

    Documents section, added menu points and index route

--- app/References/Menu/DriverMenu.php
+++ app/References/Menu/DriverMenu.php
@@ -31,6 +31,19 @@ class DriverMenu implements InterfaceMenu {
                 ),
             ),
             array(
+                'title' => 'Documents',
+                'url' => '',
+                'icon' => 'ki-notification',
+                'roles' => ['user', 'manager', 'admin'],
+                'childs' => array(
+                    array(
+                        'title' => 'All documents',
+                        'url' => route('dashboard.documents.index'),
+                        'roles' => ['user', 'manager', 'admin'],
+                    ),
+                ),
+            ),
+            array(
                 'title' => 'My cabinet',
                 'url' => '',
                 'icon' => 'ki-user',
