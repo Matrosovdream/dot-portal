91d558210b196011793cab423876394c4014954e
dotfiler <matrosovdream@gmail.com>
2025-03-31 19:25:10 +0200

    Driver show page, added profile photo into the navbar

--- resources/views/dashboard/drivers/partials/navbar.blade.php
+++ resources/views/dashboard/drivers/partials/navbar.blade.php
@@ -42,7 +42,15 @@
 
     <div class="me-7 mb-4">
         <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
-            <img src="{{ asset('assets/admin/media/avatars/300-2.png') }}" alt="image" />
+
+            @if( isset( $driver['profilePhoto']['showUrl'] ) )
+                <img src="{{ $driver['profilePhoto']['showUrl'] }}" alt="image" />
+            @else
+                <img src="{{ asset('assets/admin/media/avatars/300-2.png') }}" alt="image" />
+            @endif
+
+
+
             <div
                 class="position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-body h-20px w-20px">
             </div>
