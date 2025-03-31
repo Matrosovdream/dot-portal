9fc78f8add865dce3e4de58c87c92e5283a991de
dotfiler <matrosovdream@gmail.com>
2025-03-31 13:58:49 +0200

    Driver create page, added block for adding Profile photo

--- resources/views/dashboard/drivers/create.blade.php
+++ resources/views/dashboard/drivers/create.blade.php
@@ -5,8 +5,7 @@
     <div class="card mb-5 mb-xl-10">
 
         <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse"
-            data-bs-target="#kt_account_profile_details" aria-expanded="true"
-            aria-controls="kt_account_profile_details">
+            data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
 
             <div class="card-title m-0">
                 <h3 class="fw-bold m-0">Driver Details</h3>
@@ -16,82 +15,28 @@
 
         <div id="kt_account_settings_profile_details" class="collapse show">
 
-            <form 
-                id="kt_account_profile_details_form" 
-                class="form"
-                method="POST"
-                action="{{ route('dashboard.drivers.store') }}"
-                enctype="multipart/form-data"
-                >
+            <form id="kt_account_profile_details_form" class="form" method="POST"
+                action="{{ route('dashboard.drivers.store') }}" enctype="multipart/form-data">
 
                 @csrf
 
-                @if( $errors->any() )
-
-                    <div class="alert alert-danger p-9">
-                        <ul>
-                            @foreach( $errors->all() as $error )
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-
-                @endif
+                @include('dashboard.includes.errors.default')
 
                 <div class="card-body border-top p-9">
 
-                    @php /*
                     <div class="row mb-6">
 
-                        <label class="col-lg-4 col-form-label fw-semibold fs-6">Photo</label>
+                        <label class="col-lg-4 col-form-label fw-semibold fs-6">
+                            Profile photo
+                        </label>
 
-                        <div class="col-lg-8">
-
-                            <div class="image-input image-input-outline" data-kt-image-input="true"
-                                style="background-image: url('assets/media/svg/avatars/blank.svg')">
-  
-                                <div class="image-input-wrapper w-125px h-125px"
-                                    style="background-image: url(assets/media/avatars/300-1.jpg)"></div>
-
-                                <label
-                                    class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow"
-                                    data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change photo">
-                                    <i class="ki-duotone ki-pencil fs-7">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-
-                                    <input type="file" name="photo" accept=".png, .jpg, .jpeg" />
-                                    <input type="hidden" name="photo_remove" />
-
-                                </label>
-
-                                <span
-                                    class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow"
-                                    data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Cancel photo">
-                                    <i class="ki-duotone ki-cross fs-2">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-                                </span>
-
-                                <span
-                                    class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow"
-                                    data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove photo">
-                                    <i class="ki-duotone ki-cross fs-2">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-                                </span>
-
-                            </div>
-
-                            <div class="form-text">Allowed file types: png, jpg, jpeg.</div>
-
+                        <div class="col-lg-4 fv-row">
+                            <x-file-uploader :inputName="'profile_photo'" :value="''" :accept="'image/*'"
+                                :multiple="false" :required="false" :label="'Upload file'" :note="'Upload 1 image'"
+                                :description="''" />
                         </div>
 
                     </div>
-                    */ @endphp
 
                     <div class="row mb-6">
 
@@ -105,9 +50,8 @@
                                 class="form-select form-select-solid form-select-lg fw-semibold">
                                 <option value="">Select a Driver type</option>
 
-                                @foreach( $references['driverType']['items'] as $item )
-                                    <option 
-                                        value="{{ $item['id'] }}">
+                                @foreach($references['driverType']['items'] as $item)
+                                    <option value="{{ $item['id'] }}">
                                         {{ $item['title'] }}
                                     </option>
                                 @endforeach
@@ -149,8 +93,7 @@
                                 <div class="col-lg-6 fv-row">
                                     <input type="text" name="lastname"
                                         class="form-control form-control-lg form-control-solid mb-3 mb-lg-0"
-                                        placeholder="Last name" 
-                                        value="{{ old('lastname') }}" />
+                                        placeholder="Last name" value="{{ old('lastname') }}" />
                                 </div>
 
                             </div>
@@ -162,12 +105,8 @@
                         <label class="col-lg-4 col-form-label required fw-semibold fs-6">Phone</label>
 
                         <div class="col-lg-4 fv-row">
-                            <input 
-                                type="text" 
-                                name="phone" 
-                                class="form-control form-control-lg form-control-solid"
-                                placeholder="Phone" 
-                                value="{{ old('phone') }}" />
+                            <input type="text" name="phone" class="form-control form-control-lg form-control-solid"
+                                placeholder="Phone" value="{{ old('phone') }}" />
                         </div>
 
                     </div>
@@ -177,12 +116,8 @@
                         <label class="col-lg-4 col-form-label required fw-semibold fs-6">Email</label>
 
                         <div class="col-lg-4 fv-row">
-                            <input 
-                                type="text" 
-                                name="email" 
-                                class="form-control form-control-lg form-control-solid"
-                                placeholder="Email" 
-                                value="{{ old('email') }}" />
+                            <input type="text" name="email" class="form-control form-control-lg form-control-solid"
+                                placeholder="Email" value="{{ old('email') }}" />
                         </div>
 
                     </div>
@@ -192,10 +127,7 @@
                         <label class="col-lg-4 col-form-label required fw-semibold fs-6">DOB</label>
 
                         <div class="col-lg-4 fv-row">
-                            <input 
-                                type="date" 
-                                name="dob" 
-                                class="form-control form-control-lg form-control-solid"
+                            <input type="date" name="dob" class="form-control form-control-lg form-control-solid"
                                 value="{{ old('dob') }}" />
                         </div>
 
@@ -206,12 +138,8 @@
                         <label class="col-lg-4 col-form-label required fw-semibold fs-6">SSN</label>
 
                         <div class="col-lg-4 fv-row">
-                            <input 
-                                type="text" 
-                                name="ssn" 
-                                class="form-control form-control-lg form-control-solid"
-                                placeholder="SSN"
-                                value="{{ old('ssn') }}" />
+                            <input type="text" name="ssn" class="form-control form-control-lg form-control-solid"
+                                placeholder="SSN" value="{{ old('ssn') }}" />
                         </div>
 
                     </div>
@@ -221,16 +149,13 @@
                         <label class="col-lg-4 col-form-label required fw-semibold fs-6">Hire date</label>
 
                         <div class="col-lg-4 fv-row">
-                            <input 
-                                type="date" 
-                                name="hire_date" 
-                                class="form-control form-control-lg form-control-solid"
+                            <input type="date" name="hire_date" class="form-control form-control-lg form-control-solid"
                                 value="{{ old('hire_date') }}" />
                         </div>
 
                     </div>
 
-                    
+
 
                 </div>
 
