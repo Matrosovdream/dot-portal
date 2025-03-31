7a55f86ae06f713861759922aea2408fb888cfd0
dotfiler <matrosovdream@gmail.com>
2025-03-31 11:36:10 +0200

    DriverSeeder, added new driver by default

--- database/seeders/DriverSeeder.php
+++ database/seeders/DriverSeeder.php
@@ -50,6 +50,40 @@ class DriverSeeder extends Seeder
             ],
         ];
 
+        $drivers[] = [
+            "main" => [
+                "firstname" => "Stan",
+                "lastname" => "Matrosov",
+                "phone" => "+38111222",
+                "email" => "matrosovo@gmail.com",
+                "dob" => "2017-03-09",
+                "ssn" => "888899922",
+                "hire_date" => "2024-01-01",
+                "driver_type_id" => 1,
+                "user_id" => 3,
+            ],
+            "address" => [
+                "address1" => "123 Main St",
+                "address2" => "Apt 1",
+                "city" => "New York",
+                "state_id" => 1,
+                "zip" => "10001"
+            ],
+            "license" => [
+                "type_id" => 1,
+                "endorsement_id" => 1,
+                "license_number" => "123456789",
+                "expiration_date" => "2023-01-01",
+                "state_id" => 1,
+            ],
+            "medical" => [
+                "examiner_name" => "Dr. Smith",
+                "national_registry" => "123456789",
+                "issue_date" => "2021-01-01",
+                "expiration_date" => "2023-01-01",
+            ],
+        ];
+
         // Add driver
         foreach ($drivers as $driverSet) {
             // Create driver
