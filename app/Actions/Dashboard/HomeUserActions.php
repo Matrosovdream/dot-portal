3f8bad3d89e1e551f0fa01f3de13a5b2af0894d3
dotfiler <matrosovdream@gmail.com>
2025-04-01 18:06:07 +0200

    Dashboard home page, added stats data into the Actions class and dislayed in a template

--- app/Actions/Dashboard/HomeUserActions.php
+++ app/Actions/Dashboard/HomeUserActions.php
@@ -2,22 +2,41 @@
 namespace App\Actions\Dashboard;
 
 use App\Models\Service;
+use App\Repositories\Driver\DriverRepo;
+use App\Repositories\Vehicle\VehicleRepo;
 
 class HomeUserActions {
 
+    private $driverRepo;
+    private $vehicleRepo;
+
     public function __construct()
     {
-        // 
+        $this->driverRepo = new DriverRepo;
+        $this->vehicleRepo = new VehicleRepo;
+
     }
 
     public function index()
     {
         $data = [
             'title' => 'Services list',
+            'stats' => $this->getStats(),
             'services' => Service::paginate(10)
         ];
 
         return $data;
+    }
+
+    public function getStats() {
+
+        $stats = [
+            'drivers' => $this->driverRepo->getCompanyStats( auth()->user()->id ),
+            'vehicles' => $this->vehicleRepo->getCompanyStats( auth()->user()->id ),
+        ];
+//dd($stats);
+        return $stats;
+
     }
 
 }
