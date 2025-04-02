2c9ae231b1a86fc616ad2a68c9e876bdad1709af
dotfiler <matrosovdream@gmail.com>
2025-04-02 11:30:08 +0200

    Drivers migration, removed fields that relate to User model

--- database/migrations/2025_01_31_100909_create_drivers_table.php
+++ database/migrations/2025_01_31_100909_create_drivers_table.php
@@ -14,18 +14,18 @@ return new class extends Migration
 
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
+            $table->foreignId('user_id')->on('users');
+            $table->foreignId('company_id')->on('companies');
+            $table->integer('profile_photo_id')->nullable();
+            /*$table->string('firstname');
             $table->string('middlename')->nullable();
             $table->string('lastname');
             $table->string('phone')->nullable();
-            $table->string('email')->nullable();
+            $table->string('email')->nullable();*/
             $table->date('dob')->nullable();
             $table->string('ssn')->nullable();
             $table->date('hire_date')->nullable();
             $table->foreignId('driver_type_id')->on('ref_driver_type');
-            $table->foreignId('user_id')->on('users');
-            $table->foreignId('company_id')->on('companies');
-            $table->integer('profile_photo_id')->nullable();
             $table->timestamps();
         });
 
