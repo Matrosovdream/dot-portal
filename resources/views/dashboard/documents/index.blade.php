5770c6a53974167705deda3d682fc899e8e0cd7b
dotfiler <matrosovdream@gmail.com>
2025-04-04 10:13:22 +0200

    Dashboard documents index, added preview file feature

--- resources/views/dashboard/documents/index.blade.php
+++ resources/views/dashboard/documents/index.blade.php
@@ -5,105 +5,111 @@
 
 @section('content')
 
-<div class="card card-flush">
+    <div class="card card-flush">
 
-    <form action="{{ route('dashboard.documents.index') }}" method="GET">
+        <form action="{{ route('dashboard.documents.index') }}" method="GET">
 
-        <div class="card-header align-items-center py-5 gap-2 gap-md-5">
+            <div class="card-header align-items-center py-5 gap-2 gap-md-5">
 
-            <div class="card-title">
-                
-            </div>
+                <div class="card-title">
 
-            <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
-
-                <div class="d-flex align-items-center position-relative my-1">
-                    <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-4">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                    </i>
-                    <input 
-                        type="text" 
-                        name="q"
-                        value="{{ request()->q ?? '' }}"
-                        data-kt-ecommerce-product-filter="search" 
-                        class="form-control form-control-solid w-250px ps-12" 
-                        placeholder="Search Documents"
-                        >
                 </div>
 
-                <button type="submit" class="btn btn-primary">Filter</button>
+                <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
 
-            </div>
-        </div>
+                    <div class="d-flex align-items-center position-relative my-1">
+                        <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-4">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
+                        <input type="text" name="q" value="{{ request()->q ?? '' }}"
+                            data-kt-ecommerce-product-filter="search" class="form-control form-control-solid w-250px ps-12"
+                            placeholder="Search Documents">
+                    </div>
 
-    </form>
+                    <button type="submit" class="btn btn-primary">Filter</button>
 
-    <div class="card-body pt-0">
-        <div class="table-responsive">
-
-            @if( count($documents['items']) == 0 )
-                <div class="text-center mt-10">
-                    <h4>No documents found</h4>
                 </div>
-            @else
+            </div>
 
-                <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_products_table">
-                    <thead>
-                        <tr class="text-start text-gray-500 fw-bold fs-7 text-uppercase gs-0">
-                            <th>Name</th>
-                            <th>Tags</th>
-                            <th class="text-center">Extension</th>
-                            <th>Size</th>
-                            <th class="text-center">Added</th>
-                            <th class="text-center">Download</th>
-                        </tr>
-                    </thead>
-                    <tbody class="fw-semibold text-gray-600">
+        </form>
 
-                        @foreach($documents['items'] as $document)
+        <div class="card-body pt-0">
+            <div class="table-responsive">
 
-                            <tr>
-                                <td>
-                                    <a href="{{ $document['downloadUrl'] }}">
-                                        {{ $document['filename'] }}
-                                    </a>
-                                </td>
-                                <td>
-                                    {{ $document['tagGrouped'] }}
-                                </td>
-                                <td class="text-center">
-                                    {{ $document['extension'] }}
-                                </td>
-                                <td>
-                                    {{ $document['sizeFormatted'] }}
-                                </td>
-                                <td class="text-center pe-0">
-                                    {{ $document['Model']->created_at->format('d/m/Y') }}
-                                </td>
-                                <td class="text-center">
-                                    <a href="{{ $document['downloadUrl'] }}" class="fs-6">
-                                        <i class="fas fa-download"></i>
-                                    </a>
-                                </td>
+                @if(count($documents['items']) == 0)
+                    <div class="text-center mt-10">
+                        <h4>No documents found</h4>
+                    </div>
+                @else
+
+                    <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_products_table">
+                        <thead>
+                            <tr class="text-start text-gray-500 fw-bold fs-7 text-uppercase gs-0">
+                                <th>Name</th>
+                                <th class="text-center"></th>
+                                <th>Tags</th>
+                                <th class="text-center">Extension</th>
+                                <th>Size</th>
+                                <th class="text-center">Added</th>
+                                <th class="text-center">Download</th>
                             </tr>
+                        </thead>
+                        <tbody class="fw-semibold text-gray-600">
 
-                        @endforeach
+                            @foreach($documents['items'] as $document)
 
-                    </tbody>
-                </table>
+                                <tr>
+                                    <td>
+                                        <a href="{{ $document['downloadUrl'] }}">
+                                            {{ $document['filename'] }} (download)
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-primary btn-sm flex-shrink-0 me-3"
+                                            data-bs-toggle="modal" data-bs-target="#kt_modal_filepreview_{{ $document['id'] }}">
+                                            Preview
+                                        </a>
+                                    </td>
+                                    <td>
+                                        {{ $document['tagGrouped'] }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ $document['extension'] }}
+                                    </td>
+                                    <td>
+                                        {{ $document['sizeFormatted'] }}
+                                    </td>
+                                    <td class="text-center pe-0">
+                                        {{ $document['Model']->created_at->format('d/m/Y') }}
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="{{ $document['downloadUrl'] }}" class="fs-6">
+                                            <i class="fas fa-download"></i>
+                                        </a>
+                                    </td>
+                                </tr>
 
-            @endif
+                                @include('dashboard.modals.layout.file-preview', [
+                                    'file_id' => $document['id']
+                                ])
 
-        </div>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+
+                @endif
+
+            </div>
 
 
-        <div id="" class="row">
-            {{ $documents['Model']->links('dashboard.includes.pagination.default') }}
+            <div id="" class="row">
+                {{ $documents['Model']->links('dashboard.includes.pagination.default') }}
+            </div>
+
         </div>
 
     </div>
-
-</div>
 
 @endsection
