15d6454b80741f09369bebac1d3a717ed583958c
dotfiler <matrosovdream@gmail.com>
2025-04-07 11:29:23 +0200

    Driver drug test, new respective repo and repo injected into DriverRepo class

---
+++ app/Repositories/Driver/DriverDrugTestRepo.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Repositories\Driver;
+
+use App\Repositories\AbstractRepo;
+use App\Models\DriverDrugTest;
+use App\Repositories\File\FileRepo;
+
+class DriverDrugTestRepo extends AbstractRepo
+{
+
+    protected $model;
+
+    protected $fields = [];
+
+    protected $withRelations = [];
+
+    protected $fileRepo;
+
+    public function __construct()
+    {
+        $this->model = new DriverDrugTest();
+
+        $this->fileRepo = new FileRepo();
+
+    }
+
+    public function mapItem($item)
+    {
+
+        if( empty($item) ) {
+            return null;
+        }
+
+        $res = [
+            'id' => $item->id,
+            'driver_id' => $item->driver_id,
+            'test_type' => $item->test_type,
+            'test_date' => $item->test_date,
+            'result' => $item->result,
+            'file' => $this->fileRepo->mapItem($item->file),
+            'Model' => $item
+        ];
+
+        return $res;
+    }
+
+}
