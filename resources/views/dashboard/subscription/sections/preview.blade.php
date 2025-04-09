4d825de5e07ec93091ba65d425b1a49771942b98
dotfiler <matrosovdream@gmail.com>
2025-04-09 13:15:01 +0200

    Subscription preview page, changed layout and made some real validated data

--- resources/views/dashboard/subscription/sections/preview.blade.php
+++ resources/views/dashboard/subscription/sections/preview.blade.php
@@ -2,6 +2,7 @@
 
     <div class="card-body">
 
+        <!--
         <div class="notice d-flex bg-light-warning rounded border-warning border border-dashed mb-12 p-6">
 
             <i class="ki-duotone ki-information fs-2tx text-warning me-4">
@@ -23,12 +24,13 @@
             </div>
 
         </div>
+        -->
 
         <div class="row">
             <div class="col-lg-7">
 
                 <h3 class="mb-2">
-                    Active until 
+                    Active until
                     {{ date('M d, Y', strtotime($subscription['end_date'])) }}
                 </h3>
                 <p class="fs-6 text-gray-600 fw-semibold mb-6 mb-lg-15">
@@ -39,7 +41,13 @@
                     <span class="text-gray-800 fw-bold me-1">
                         ${{ $subscription['subscription']['price'] }}
                     </span>
-                    <span class="text-gray-600 fw-semibold">Per Month</span>
+                    <span class="text-gray-600 fw-semibold">
+                        @if( $subscription['subscription']['duration'] == 'monthly' )
+                            Per Month
+                        @elseif( $subscription['subscription']['duration'] == 'yearly' )
+                            Per Year
+                        @endif
+                    </span>
                 </div>
 
                 <div class="fs-6 text-gray-600 fw-semibold">
@@ -51,16 +59,25 @@
             <div class="col-lg-5">
 
                 <div class="d-flex text-muted fw-bold fs-5 mb-3">
-                    <span class="flex-grow-1 text-gray-800">Users</span>
-                    <span class="text-gray-800">86 of 100 Used</span>
+                    <span class="flex-grow-1 text-gray-800">Drivers</span>
+                    <span class="text-gray-800">
+                        {{ $subscription['driversUsed'] }} of {{ $subscription['subscription']['drivers_amount'] }} Used
+                    </span>
                 </div>
 
                 <div class="progress h-8px bg-light-primary mb-2">
-                    <div class="progress-bar bg-primary" role="progressbar" style="width: 86%" aria-valuenow="86"
-                        aria-valuemin="0" aria-valuemax="100"></div>
+                    <div 
+                        class="progress-bar bg-primary" 
+                        role="progressbar" 
+                        style="width: {{ $subscription['driversUsedPercent']  }}%" 
+                        ria-valuenow="{{ $subscription['driversUsedPercent']  }}"
+                        aria-valuemin="0" 
+                        aria-valuemax="100"
+                        ></div>
                 </div>
 
-                <div class="fs-6 text-gray-600 fw-semibold mb-10">14 Users remaining until your plan requires update
+                <div class="fs-6 text-gray-600 fw-semibold mb-10">
+                    {{ $subscription['driversRemained'] }} Users remaining until your plan requires update
                 </div>
 
                 <div class="d-flex justify-content-end pb-0 px-0">
