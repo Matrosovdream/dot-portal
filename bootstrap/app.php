30ff4952f3d92bd60e3f426a31d5e2d4e757d95a
dotfiler <matrosovdream@gmail.com>
2025-04-11 09:32:11 +0200

    Bootstrap app, removed unnecessary middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 use App\Http\Middleware\isAdmin;
 use App\Http\Middleware\isUser;
-use App\Http\Middleware\isUserOrder;
 use App\Http\Middleware\hasRole;
-use App\Http\Middleware\isOrderPaid;
-use App\Http\Middleware\isUserCart;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,9 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'isAdmin' => isAdmin::class,
             'isUser' => isUser::class,
             'hasRole' => hasRole::class,
-            'isUserOrder' => isUserOrder::class,
-            'isOrderPaid' => isOrderPaid::class,
-            'isUserCart' => isUserCart::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
