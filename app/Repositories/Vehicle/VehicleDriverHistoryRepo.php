6213942f09ca17f709ae0ae8d6cc5595d4a330b0
dotfiler <matrosovdream@gmail.com>
2025-04-16 00:25:00 +0200

    VehicleDriverHistoryRepo, new variable driver derived from driverRepo

--- app/Repositories/Vehicle/VehicleDriverHistoryRepo.php
+++ app/Repositories/Vehicle/VehicleDriverHistoryRepo.php
@@ -3,7 +3,7 @@ namespace App\Repositories\Vehicle;
 
 use App\Repositories\AbstractRepo;
 use App\Models\VehicleDriverHistory;
-use App\Repositories\File\FileRepo;
+use App\Repositories\Driver\DriverRepo;
 
 
 class VehicleDriverHistoryRepo extends AbstractRepo
@@ -11,10 +11,13 @@ class VehicleDriverHistoryRepo extends AbstractRepo
 
     protected $model;
     protected $fields = [];
+    protected $driverRepo;
 
     public function __construct()
     {
         $this->model = new VehicleDriverHistory();
+
+        $this->driverRepo = new DriverRepo;
     }
 
     public function mapItem($item)
@@ -26,10 +29,11 @@ class VehicleDriverHistoryRepo extends AbstractRepo
             'id' => $item->id,
             'vehicle_id' => $item->vehicle_id,
             'driver_id' => $item->driver_id,
+            'driver' => $this->driverRepo->mapItem( $item->driver ),
             'start_date' => $item->start_date,
             'end_date' => $item->end_date,
             'Model' => $item,
-        ];
+        ]; 
         return $res;
     }
 
