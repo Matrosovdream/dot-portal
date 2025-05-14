77e6445ffd01c37bb9778d6a220ddd4e01b33705
Stan Matrosov <matrosovdream@gmail.com>
2025-05-14 15:36:30 +0200

    UserPaymentCardSeeder, added payment method to card parameters

--- database/seeders/UserPaymentCardSeeder.php
+++ database/seeders/UserPaymentCardSeeder.php
@@ -15,8 +15,8 @@ class UserPaymentCardSeeder extends Seeder
     {
         
         $subs = [
-            ['user_id' => 3, 'card_number' => '411112254532', 'card_holder_name' => 'Stan Matrosov', 'expiry_date' => '2025-01-01', 'primary' => true],
-            ['user_id' => 3, 'card_number' => '411212259855', 'card_holder_name' => 'Stan Matrosov', 'expiry_date' => '2027-03-01'],
+            ['user_id' => 3, 'card_number' => '411112254532', 'card_holder_name' => 'Stan Matrosov', 'expiry_date' => '2025-01-01', 'payment_method_id' => 1, 'primary' => true],
+            ['user_id' => 3, 'card_number' => '411212259855', 'card_holder_name' => 'Stan Matrosov', 'expiry_date' => '2027-03-01', 'payment_method_id' => 1,],
         ];
         
         foreach ($subs as $item) {
