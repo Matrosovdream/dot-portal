3df3da8669f7be6f3087cdc084cc8d0165027961
Stan Matrosov <matrosovdream@gmail.com>
2025-05-19 15:00:31 +0200

    DatabaseSeeder, turn of UserPaymentHistorySeeder()

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             //UserSubscriptionSeeder::class,
             //UserPaymentCardSeeder::class,
-            UserPaymentHistorySeeder::class,
+            //UserPaymentHistorySeeder::class,
 
             // Referecences
             LanguageSeeder::class,
