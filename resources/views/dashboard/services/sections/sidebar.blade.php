90edc8e11a5a9f93170ca027970033e188c849e4
Stan Matrosov <matrosovdream@gmail.com>
2025-05-21 11:05:08 +0200

    Service single page, added the form for changing Status

--- resources/views/dashboard/services/sections/sidebar.blade.php
+++ resources/views/dashboard/services/sections/sidebar.blade.php
@@ -9,21 +9,26 @@
 				</div>
 			</div>
 
-			<div class="card-body pt-0">
+			<form method="POST" action="{{ route('dashboard.services.updatestatus', $service['id']) }}" enctype="multipart/form-data">
+				@csrf
 
-				<select class="form-select mb-2" data-control="select2" data-hide-search="true"
-					data-placeholder="Select an option" id="kt_ecommerce_add_product_status_select" name="status">
-					<option value="published" @if( 1 == 1 ) selected="selected" @endif >Published</option>
-					<option value="draft" @if( 1 == 1 ) selected="selected" @endif>Draft</option>
-				</select>
+				<div class="card-body pt-0">
 
-			</div>
+					<select class="form-select mb-2" data-control="select2" data-hide-search="true"
+						data-placeholder="Select an option" id="kt_ecommerce_add_product_status_select" name="status">
+						<option value="1" @if( $service['status_id'] == 1 ) selected="selected" @endif >Published</option>
+						<option value="2" @if( $service['status_id'] == 2 ) selected="selected" @endif>Draft</option>
+					</select>
 
-			<div class="d-flex justify-content-center mt-30">
-				<button type="submit" id="kt_ecommerce_add_product_submit" class="btn btn-primary">
-					<span class="indicator-label">Save Changes</span>
-				</button>
-			</div>
+				</div>
+
+				<div class="d-flex justify-content-center mt-30">
+					<button type="submit" id="kt_ecommerce_add_product_submit" class="btn btn-primary">
+						<span class="indicator-label">Save Changes</span>
+					</button>
+				</div>
+
+			</form>
 
 		</div>
 
