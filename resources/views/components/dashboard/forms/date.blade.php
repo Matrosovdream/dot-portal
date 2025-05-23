e867d91de1b5a30e54954ed3e86e998403e1c5e3
Stan Matrosov <matrosovdream@gmail.com>
2025-05-23 16:09:56 +0200

    Date component, ready version

---
+++ resources/views/components/dashboard/forms/date.blade.php
@@ -0,0 +1,24 @@
+<div class="col-md-6 fv-row fv-plugins-icon-container mb-5">
+    @if($label)
+        <label for="{{ $inputId ?? $inputName }}" class="form-label {{ $required ? 'required' : '' }} fs-5 fw-semibold mb-2">
+            {{ $label }}
+        </label>
+    @endif
+
+    <input
+        type="text"
+        class="form-control form-control-solid datepicker"
+        id="{{ $inputId ?? $inputName }}"
+        name="{{ $inputName }}"
+        value="{{ $value ?? '' }}"
+        {{ $required ? 'required' : '' }}
+    >
+
+    @if($note)
+        <div class="form-text">{{ $note }}</div>
+    @endif
+
+    @if($description)
+        <div class="text-muted fs-7">{{ $description }}</div>
+    @endif
+</div>
