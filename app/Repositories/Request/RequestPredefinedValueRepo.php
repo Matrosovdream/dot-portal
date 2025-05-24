3cc9869b62b29996af17309336bf2b27268c3e3d
Stan Matrosov <matrosovdream@gmail.com>
2025-05-24 23:16:32 +0200

    New Repo RequestPredefinedValue

---
+++ app/Repositories/Request/RequestPredefinedValueRepo.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Repositories\Request;
+
+use App\Repositories\AbstractRepo;
+use App\Models\RequestPredefinedValue;
+
+
+class RequestPredefinedValueRepo extends AbstractRepo
+{
+
+    protected $model;
+
+    protected $fields = [];
+
+    public function __construct()
+    {
+        $this->model = new RequestPredefinedValue();
+    }
+
+    public function syncValue($request_id, $field_id, $value)
+    {
+        $data = $this->model->where('request_id', $request_id)->where('field_id', $field_id)->first();
+
+        if (empty($data)) {
+            $data = new RequestFieldValue();
+            $data->request_id = $request_id;
+            $data->field_id = $field_id;
+        }
+        $data->value = $value;
+        $data->save();
+    }
+
+    public function findValue($request_id, $field_id)
+    {
+        $data = $this->model->where('request_id', $request_id)->where('field_id', $field_id)->first();
+        return $this->mapItem($data);
+    }
+
+    public function mapItem($item)  
+    {
+        if (empty($item)) {
+            return null;
+        }
+
+        $res = [
+            'id' => $item->id,
+            'request_id' => $item->request_id,
+            'field' => $this->refFormFieldRepo->mapItem($item->field),
+            'value' => $item->value,
+            'Model' => $item
+        ];
+        return $res;
+    }
+
+}
