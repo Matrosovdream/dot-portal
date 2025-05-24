4551d75afc30ab984722992c356c35a5881b1e1e
Stan Matrosov <matrosovdream@gmail.com>
2025-05-24 22:29:49 +0200

    Request show page, new form blank for predefined path

---
+++ resources/views/dashboard/servicerequest/sections/submit-form-predefined.blade.php
@@ -0,0 +1,28 @@
+<form action="{{ route('dashboard.servicerequest.store.request', ['group' => $group['slug'], 'service' => $service['slug']]) }}"
+    class="form mb-15 fv-plugins-bootstrap5 fv-plugins-framework" method="POST">
+    @csrf
+
+    @include('dashboard.includes.errors.default')
+
+    <div class="row mb-5">
+
+        @include( $formPath )
+        
+    </div>
+
+    <div class="separator mb-8"></div>
+
+    <button type="submit" class="btn btn-primary" id="kt_careers_submit_button">
+
+        <span class="indicator-label">Apply Now</span>
+
+        <span class="indicator-progress">Please wait...
+            <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+        </span>
+
+    </button>
+
+</form>
+
+
+
