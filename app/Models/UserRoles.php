2f245e373a48b0a3b3c5fe4460ebe90fd8ca3144
Stan Matrosov <matrosovdream@gmail.com>
2025-05-26 19:06:33 +0200

    UserRoles Model, removed support of hasFactory

--- app/Models/UserRoles.php
+++ app/Models/UserRoles.php
@@ -2,14 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserRoles extends Model
 {
-
-    use HasFactory;
-
     protected $table = 'user_roles';
 
     protected $fillable = [
