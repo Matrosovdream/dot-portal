909f11322b1ff0760086f633dabb66ace3e76da6
Stan Matrosov <matrosovdream@gmail.com>
2025-05-27 19:07:56 +0200

    Vehicles show page, added support of Datepicker

--- resources/views/dashboard/vehicles/sections/profile.blade.php
+++ resources/views/dashboard/vehicles/sections/profile.blade.php
@@ -134,7 +134,7 @@
                 <label class="col-lg-4 col-form-label required fw-semibold fs-6">Registration expire date</label>
 
                 <div class="col-lg-4 fv-row">
-                    <input type="date" name="reg_expire_date" class="form-control form-control-lg form-control-solid"
+                    <input type="date" name="reg_expire_date" class="form-control form-control-lg form-control-solid datepicker"
                         value="{{ $vehicle['regExpireDate'] }}" />
                 </div>
 
@@ -146,7 +146,7 @@
 
                 <div class="col-lg-4 fv-row">
                     <input type="date" name="inspection_expire_date"
-                        class="form-control form-control-lg form-control-solid"
+                        class="form-control form-control-lg form-control-solid datepicker"
                         value="{{ $vehicle['inspectionExpireDate'] }}" />
                 </div>
 
