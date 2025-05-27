0c1657730f5cb0d83308e402eeab64bef4dc5ef6
Stan Matrosov <matrosovdream@gmail.com>
2025-05-27 13:53:43 +0200

    adminSettingsHelper, setActiveMenus() method - fixed some bugs with Active points

--- app/Helpers/adminSettingsHelper.php
+++ app/Helpers/adminSettingsHelper.php
@@ -25,30 +25,47 @@ class adminSettingsHelper
     {
         $currentUrl = request()->url();
         $currentRoute = request()->route() ? request()->route()->getName() : null;
-        
+
+        // Set active false for all items first
         foreach ($menu as $key => $item) {
             $menu[$key]['active'] = false;
+            if (isset($item['childs'])) {
+                foreach ($item['childs'] as $key2 => $item2) {
+                    $menu[$key]['childs'][$key2]['active'] = false;
+                }
+            }
+        }
+        
+        // Iterate through the menu items to find the active one
+        foreach ($menu as $key => $item) {
             $bestMatchLength = 0;
 
             if (isset($item['childs'])) {
                 foreach ($item['childs'] as $key2 => $item2) {
-                    $menu[$key]['childs'][$key2]['active'] = false;
 
                     $isExactRoute = !empty($item2['route']) && $currentRoute === $item2['route'];
+                    $isExactUrl = !empty($item2['url']) && $currentUrl === $item2['url'];
                     $isUrlPrefix = !empty($item2['url']) && str_starts_with($currentUrl, $item2['url']);
 
-                    // Prefer route match or longest URL match
-                    if ($isExactRoute || ($isUrlPrefix && strlen($item2['url']) > $bestMatchLength)) {
+                    if( $isExactUrl ) {
                         $bestMatchLength = strlen($item2['url']);
                         $bestMatchIndex = $key2;
                         $menu[$key]['active'] = true;
+                        break;
+                    } elseif( $isUrlPrefix ) {
+                        $bestMatchLength = strlen($item2['url']);
+                        $bestMatchIndex = $key2;
+                        $menu[$key]['active'] = true;
+                        break;
                     }
+
                 }
 
                 // Mark only the best matching child as active
                 if (isset($bestMatchIndex)) {
                     $menu[$key]['childs'][$bestMatchIndex]['active'] = true;
                 }
+                
 
             } else {
                 if (!empty($item['url']) && str_starts_with($currentUrl, $item['url'])) {
@@ -57,7 +74,9 @@ class adminSettingsHelper
             }
         }
 
-        //dd($menu[5]['childs']);
+        if( request()->has('lggg') ) {
+            dd($menu[5]['childs']);
+        }
 
         return $menu;
     }
