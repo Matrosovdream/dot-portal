9751ffa97b65a9f04b559b6dbb7d811d83ebccd1
Stan Matrosov <matrosovdream@gmail.com>
2025-05-27 15:11:58 +0200

    adminSettingsHelper, setActiveMenus() method - final fix of all bugs with Active points

--- app/Helpers/adminSettingsHelper.php
+++ app/Helpers/adminSettingsHelper.php
@@ -35,28 +35,23 @@ class adminSettingsHelper
                 }
             }
         }
-        
+
         // Iterate through the menu items to find the active one
         foreach ($menu as $key => $item) {
             $bestMatchLength = 0;
+            $bestMatchIndex = null;
 
             if (isset($item['childs'])) {
                 foreach ($item['childs'] as $key2 => $item2) {
 
-                    $isExactRoute = !empty($item2['route']) && $currentRoute === $item2['route'];
+                    $isExactRoute = (!empty($item2['route']) && $currentRoute === trim($item2['route']));
                     $isExactUrl = !empty($item2['url']) && $currentUrl === $item2['url'];
                     $isUrlPrefix = !empty($item2['url']) && str_starts_with($currentUrl, $item2['url']);
 
-                    if( $isExactUrl ) {
+                    if( $isExactUrl || $isUrlPrefix ) {
                         $bestMatchLength = strlen($item2['url']);
                         $bestMatchIndex = $key2;
                         $menu[$key]['active'] = true;
-                        break;
-                    } elseif( $isUrlPrefix ) {
-                        $bestMatchLength = strlen($item2['url']);
-                        $bestMatchIndex = $key2;
-                        $menu[$key]['active'] = true;
-                        break;
                     }
 
                 }
@@ -75,13 +70,10 @@ class adminSettingsHelper
         }
 
         if( request()->has('lggg') ) {
-            dd($menu[5]['childs']);
+            dd($menu[2]['childs']);
         }
 
         return $menu;
     }
-
-
-
 
 }
