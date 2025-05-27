d67cc30bc67eae9e25299565d4351c50e12ae836
Stan Matrosov <matrosovdream@gmail.com>
2025-05-27 15:24:18 +0200

    Profile page, hid Address block in a navbar

--- resources/views/dashboard/profile/partials/navbar.blade.php
+++ resources/views/dashboard/profile/partials/navbar.blade.php
@@ -68,14 +68,20 @@
                     </a>
                 </div>
 
+                
+
                 <div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
-                    <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
-                        <i class="ki-duotone ki-geolocation fs-4 me-1">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                        </i>
-                        {{ $user['address']['city'] ?? '' }}, {{ $user['address']['zip'] ?? '' }}
-                    </a>
+
+                    @if( isset($user['address']) )
+                        <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2">
+                            <i class="ki-duotone ki-geolocation fs-4 me-1">
+                                <span class="path1"></span>
+                                <span class="path2"></span>
+                            </i>
+                            {{ $user['address']['city'] ?? '' }}, {{ $user['address']['zip'] ?? '' }}
+                        </a>
+                    @endif
+
                     <a href="#" class="d-flex align-items-center text-gray-500 text-hover-primary mb-2">
                         <i class="ki-duotone ki-sms fs-4 me-1">
                             <span class="path1"></span>
