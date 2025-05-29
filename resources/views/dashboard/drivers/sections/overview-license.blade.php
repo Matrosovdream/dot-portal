7053839b55fadb0d8011b606a2b171a83806d9fd
Stan Matrosov <matrosovdream@gmail.com>
2025-05-29 16:38:37 +0200

    Drivers show page, changed layout for all Document values

--- resources/views/dashboard/drivers/sections/overview-license.blade.php
+++ resources/views/dashboard/drivers/sections/overview-license.blade.php
@@ -67,7 +67,7 @@
 
                     @if( $file )
                         <a href="{{ $file['file']['downloadUrl'] }}">
-                            {{ $file['title'] }}
+                            Download ({{ $file['title'] }})
                         </a>
                     @else
                         -
