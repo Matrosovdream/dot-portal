435102aacab40e756bf01def2bbd8dfcbb1bc554
Stan Matrosov <matrosovdream@gmail.com>
2025-06-01 22:57:12 +0200

    RequestFieldValueRepo, new method uploadField and injected into syncValue

--- app/Repositories/Request/RequestFieldValueRepo.php
+++ app/Repositories/Request/RequestFieldValueRepo.php
@@ -4,12 +4,17 @@ namespace App\Repositories\Request;
 use App\Repositories\AbstractRepo;
 use App\Repositories\References\RefFormFieldRepo;
 use App\Models\RequestFieldValue;
+use App\Mixins\File\FileStorage;
+
 
 
 class RequestFieldValueRepo extends AbstractRepo
 {
 
     private $refFormFieldRepo;
+
+    protected $fileStorage;
+
     protected $model;
 
     protected $fields = ['field'];
@@ -20,11 +25,25 @@ class RequestFieldValueRepo extends AbstractRepo
 
         // References
         $this->refFormFieldRepo = new RefFormFieldRepo();
+
+        $this->fileStorage = new FileStorage();
     }
 
     public function syncValue($request_id, $field_id, $value)
     {
         $data = $this->model->where('request_id', $request_id)->where('field_id', $field_id)->first();
+
+        // Field data
+        $field = $this->refFormFieldRepo->getById($field_id);
+
+        // Upload file handling
+        if( $field['type'] == 'file' ) {
+            $file = $this->uploadFile(
+                $request_id,
+                $field['slug'],
+                ['request '.$request_id, 'file']
+            );
+        }
 
         if (empty($data)) {
             $data = new RequestFieldValue();
@@ -39,6 +58,29 @@ class RequestFieldValueRepo extends AbstractRepo
     {
         $data = $this->model->where('request_id', $request_id)->where('field_id', $field_id)->first();
         return $this->mapItem($data);
+    }
+
+    public function uploadFile( $request_id, $filename, $tags = [] ) {
+
+        $file = $this->fileStorage->uploadFile(
+            $filename, 
+            'fieldValue/' . $request_id,
+            'local',
+            ['tags' => $tags]
+        );
+
+        if( isset($file['file']['id']) ) {
+            $this->update(
+                $request_id,
+                [
+                    'file_id' => $file['file']['id'],
+                ]
+            );
+            return $file['file']['id'];
+        }
+
+        return null;
+
     }
 
     public function mapItem($item)  
