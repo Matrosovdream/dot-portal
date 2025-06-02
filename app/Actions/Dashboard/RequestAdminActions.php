0ada6a9dc454706375dfbfbb377fdee9fad4e3aa
Stan Matrosov <matrosovdream@gmail.com>
2025-06-02 14:28:36 +0200

    RequestAdminActions, added companyDrivers to references

--- app/Actions/Dashboard/RequestAdminActions.php
+++ app/Actions/Dashboard/RequestAdminActions.php
@@ -8,6 +8,7 @@ use App\Repositories\Request\RequestRepo;
 use App\Repositories\References\RefRequestStatusRepo;
 use App\Repositories\User\UserPaymentHistoryRepo;
 use App\References\ServiceReferences;
+use App\Repositories\Driver\DriverRepo;
 
 class RequestAdminActions {
 
@@ -18,6 +19,7 @@ class RequestAdminActions {
     private $refRequestStatus;
     private $userPaymentHistoryRepo;
     private $serviceRef;
+    private $driverRepo;
 
     public function __construct()
     {
@@ -29,6 +31,7 @@ class RequestAdminActions {
         // References
         $this->refRequestStatus = new RefRequestStatusRepo();
         $this->serviceRef = new ServiceReferences();
+        $this->driverRepo = new DriverRepo();
     }
 
     public function index()
@@ -56,7 +59,7 @@ class RequestAdminActions {
             'paymentHistory' => $payments ?? [],
             'formType' => $request['service']['form_type'] ?? 'custom',
             'formId' => $request['service']['form_id'] ?? null,
-            'references' => $this->getReferences()
+            'references' => $this->getReferences( $request['user']['id'] ?? null )
         ];
 
         if( $request['service']['form_type'] == 'predefined' ) {
@@ -99,10 +102,14 @@ class RequestAdminActions {
         return $this->requestRepo->delete($group_id);
     }
 
-    public function getReferences()
+    public function getReferences( $user_id=null )
     {
         return [
             'requestStatus' => $this->refRequestStatus->getAll(),
+            /*'companyDrivers' => $this->driverRepo->getAll(
+                ['company_id' => $user_id], 
+                $paginate = 1000
+            ),*/
         ];
     }
 
