1a2488b020604227dbc1ea9d872a255cd1381397
Stan Matrosov <matrosovdream@gmail.com>
2025-06-02 10:28:04 +0200

    FileStorage, fixes for the array of input files

--- app/Repositories/Request/RequestFieldValueRepo.php
+++ app/Repositories/Request/RequestFieldValueRepo.php
@@ -40,7 +40,7 @@ class RequestFieldValueRepo extends AbstractRepo
         if( $field['type'] == 'file' ) {
             $file = $this->uploadFile(
                 $request_id,
-                '[fields]['.$field['id'].']',
+                'fields.'.$field['id'],
                 ['request '.$request_id, 'file']
             );
         }
