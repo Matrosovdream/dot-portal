b998ea68d557d9ceb65b3864e4349db34b45a49e
Stan Matrosov <matrosovdream@gmail.com>
2025-06-03 17:23:10 +0200

    AdminNotificationsTest, test_update_record() - new method

--- tests/Feature/Admin/AdminNotificationsTest.php
+++ tests/Feature/Admin/AdminNotificationsTest.php
@@ -11,7 +11,7 @@ class AdminNotificationsTest extends TestCase
 {
 
     use EntityAbstract;
-    
+
     protected $initialPath = '/dashboard/notifications-manager';
     protected $user_id = 1;
     protected $user;
@@ -56,6 +56,7 @@ class AdminNotificationsTest extends TestCase
     {
         // Prepare test data
         $data = [
+            'user_id' => $this->user_id, 
             'type' => 1,
             'title' => 'recordTest',
             'message' => 'message test',
@@ -71,11 +72,47 @@ class AdminNotificationsTest extends TestCase
         // Verify the record exists
         $record = $this->findRecord($data);
 
+        // Assert
         $this->assertNotNull($record, 'Record was not created successfully.');
 
         if ($record) {
             $this->createdRecords[] = $record;
         }
+    }
+
+    public function test_update_record(): void
+    {
+
+        $data = [
+            'user_id' => $this->user_id,
+            'type' => 1,
+            'title' => 'recordTest',
+            'message' => 'message test',
+        ];
+
+        // Create a record to update
+        $record = $this->createRecord( $data );
+
+        // Prepare updated data
+        $dataUpdated = [
+            'type' => 2,
+            'title' => 'updatedRecordTest',
+            'message' => 'updated message test',
+        ];
+
+        // Perform the PUT request
+        $response = $this->actingAs($this->user)->
+            post(
+                route('dashboard.notifications-manage.update', $record->id), 
+                $dataUpdated
+        );
+
+        // Verify the record was updated
+        $updatedRecord = $this->findRecord($dataUpdated);
+
+        // Assert
+        $this->assertNotNull($updatedRecord, 'Record was not updated successfully.');
+
     }
 
 }
