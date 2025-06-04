041d584b88f870f9ba4008db66cc47de01c1137b
Stan Matrosov <matrosovdream@gmail.com>
2025-06-04 15:24:45 +0200

    UserTask Model, initial version

---
+++ app/Models/UserTask.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Metaable;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTask extends Model
+{
+
+    use Metaable;
+    
+    protected $fillable = [
+        'user_id',
+        'assigned_to',
+        'title',
+        'description',
+        'category',
+        'subcategory',
+        'status',
+        'due_date',
+        'completed_at',
+        'priority',
+        'link',
+        'entity',
+        'entity_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function userAssigned()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function meta()
+    {
+        return $this->hasMany(UserTaskMeta::class, 'task_id');
+    }
+
+}
