262973aafa52fc4d93fcbd0805e512a392e84e64
Stan Matrosov <matrosovdream@gmail.com>
2025-06-05 12:18:21 +0200

    New controller ToDoController()

---
+++ app/Http/Controllers/Dashboard/ToDoController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Actions\Dashboard\ToDoActions;
+
+class ToDoController extends Controller
+{
+
+    private $todoActions;
+
+    public function __construct()
+    {
+        $this->todoActions = new ToDoActions();
+    }
+
+    public function index( Request $request )
+    {
+
+        return view(
+            'dashboard.todo.index', 
+            $this->todoActions->index()
+        );
+
+    }
+
+    public function show( $task_id )
+    {
+        return view(
+            'dashboard.todo.show', 
+            $this->todoActions->show($task_id)
+        );
+    }
+
+}
