10836f7619720cad4134ed70b35e38592b92b07e
Stan Matrosov <matrosovdream@gmail.com>
2025-06-05 17:03:03 +0200

    SaferwebAPI, made property  inside the class

--- app/Mixins/Integrations/SaferwebAPI.php
+++ app/Mixins/Integrations/SaferwebAPI.php
@@ -9,35 +9,42 @@ class SaferwebAPI
 {
     protected string $baseUrl = 'https://saferwebapi.com/';
     protected string $apiKey;
+    protected $routes = [
+        'company.snapshot' => '/v2/usdot/snapshot/',
+        'inspection.history' => 'v3/history/inspection/',
+        'crash.history' => 'v3/history/crash/',
+        'inspection.summary' => 'v3/history/inspection/',
+        'history.all' => 'v3/history/everything/',
+    ]; 
 
     public function __construct()
     {
         $this->apiKey = config('services.saferweb.api_key');
     }
 
+    public function getCompanySnapshot(string $usdot): array|null
+    {
+        return $this->request("{$this->routes['company.snapshot']}{$usdot}");
+    }
+
     public function getInspectionHistory(string $usdot): array|null
     {
-        return $this->request("v3/history/inspection/{$usdot}");
+        return $this->request("{$this->routes['inspection.history']}{$usdot}");
     }
 
     public function getCrashHistory(string $usdot): array|null
     {
-        return $this->request("v3/history/crash/{$usdot}");
+        return $this->request("{$this->routes['crash.history']}{$usdot}");
     }
 
     public function getInspectionSummary(string $usdot): array|null
     {
-        return $this->request("v3/history/inspection/{$usdot}");
-    }
-
-    public function getCompanySnapshot(string $usdot): array|null
-    {
-        return $this->request("/v2/usdot/snapshot/{$usdot}");
+        return $this->request("{$this->routes['inspection.summary']}{$usdot}");
     }
 
     public function getHistoryAll(string $usdot): array|null
     {
-        return $this->request("v3/history/everything/{$usdot}");
+        return $this->request("{$this->routes['history.all']}{$usdot}");
     }
 
     protected function request(string $endpoint): array|null
