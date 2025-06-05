2d94ec330dba0b7ff86b4394f8766542bd972ca7
Stan Matrosov <matrosovdream@gmail.com>
2025-06-05 12:41:45 +0200

    ToDoActions, initial version

---
+++ app/Actions/Dashboard/ToDoActions.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Actions\Dashboard;
+
+use App\Models\Service;
+use App\Repositories\User\UserTaskRepo;
+
+
+class ToDoActions {
+
+    private $todoRepo;
+
+    public function __construct()
+    {
+        $this->todoRepo = new UserTaskRepo();
+
+    }
+
+    public function index()
+    {
+        $data = [
+            'title' => 'To-Do List',
+            'tasks' => $this->todoRepo->getAll( 
+                ['assigned_to' => auth()->user()->id],
+                $paginate = 10
+            ),
+        ];
+
+        return $data;
+    }
+
+    public function show( $task_id )
+    {
+        $task = $this->todoRepo->getByID( $task_id );
+
+        if( !$task ) {
+            abort(404, 'Task not found');
+        }
+
+        return [
+            'title' => 'Task Details #' . $task['id'],
+            'task' => $task,
+        ];
+    }
+
+}
