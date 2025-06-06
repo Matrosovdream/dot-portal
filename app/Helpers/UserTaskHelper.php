fc956874b2a2e5c889b4adf8477a6c130fb7b245
Stan Matrosov <matrosovdream@gmail.com>
2025-06-06 09:40:01 +0200

    UserTaskHelper, new method updateCompanySnapshot() and injected into UpdateUserFromSaferweb

--- app/Helpers/UserTaskHelper.php
+++ app/Helpers/UserTaskHelper.php
@@ -2,8 +2,27 @@
 
 namespace App\Helpers;
 
+use App\Mixins\Integrations\SaferwebAPI;
+use App\Repositories\User\UserRepo;
+
 class UserTaskHelper {
 
-    
+    public function updateCompanySnapshot(int $user_id): void
+    {
+
+        $apiService = new SaferwebAPI();
+        $userRepo = new UserRepo();
+
+        $user = $userRepo->getByID($user_id);
+        $dotNumber = $user['company']['dot_number'] ?? null;
+
+        $apiData = $apiService->getCompanySnapshot($dotNumber);
+
+        if ($apiData != null) { 
+            $user['Model']->company->update([
+                'mc_number' => $apiData['phone'],
+            ]);
+        }
+    }
 
 }
