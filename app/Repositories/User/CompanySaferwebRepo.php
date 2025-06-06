d703f11e35cbb59e5b5e3cb3e787a44b5510f122
Stan Matrosov <matrosovdream@gmail.com>
2025-06-06 14:47:16 +0200

    new repo CompanySaferwebRepo

---
+++ app/Repositories/User/CompanySaferwebRepo.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Repositories\User;
+
+use App\Repositories\AbstractRepo;
+use App\Models\CompanySaferweb;
+
+
+
+class CompanySaferwebRepo extends AbstractRepo
+{
+
+    protected $countryStateRepo;
+
+    protected $model;
+
+    protected $fields = [];
+
+    protected $withRelations = [];
+
+    public function __construct()
+    {
+        $this->model = new CompanySaferweb();
+
+    }
+
+    public function mapItem($item)
+    {
+
+        if( empty($item) ) {
+            return null;
+        }
+
+        $res = [
+            'id' => $item->id,
+            'user_id' => $item->user_id,
+            'company_id' => $item->company_id,
+            'dot_number' => $item->dot_number,
+            'mc_number' => $item->mc_number,
+            'legal_name' => $item->legal_name,
+            'dba_name' => $item->dba_name,
+            'entity_type' => $item->entity_type,
+            'physical_address' => $item->physical_address,
+            'mailing_address' => $item->mailing_address,
+            'latest_update' => $item->latest_update,
+            'Model' => $item
+        ];
+
+        return $res;
+    }
+
+}
