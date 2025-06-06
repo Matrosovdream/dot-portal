36b97a8a108df9bfa11a0da109c403650d1ece8e
Stan Matrosov <matrosovdream@gmail.com>
2025-06-06 14:43:37 +0200

    new migration company_saferweb

---
+++ database/migrations/2025_06_06_123638_create_company_saferweb_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('company_saferweb', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->on('users')->nullable();
+            $table->foreignId('company_id')->on('user_company')->nullable();
+            $table->string('dot_number')->nullable();
+            $table->string('mc_number')->nullable();
+            $table->string('legal_name')->nullable();
+            $table->string('dba_name')->nullable();
+            $table->string('entity_type')->nullable();
+            $table->string('physical_address')->nullable();
+            $table->string('mailing_address')->nullable();
+            $table->date('latest_update')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('company_saferweb');
+    }
+};
