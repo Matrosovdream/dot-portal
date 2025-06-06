018f31b4666aa8bdc92fe0cfc344651feb78715d
Stan Matrosov <matrosovdream@gmail.com>
2025-06-06 14:49:09 +0200

    new model CompanySaferweb

---
+++ app/Models/CompanySaferweb.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanySaferweb extends Model
+{
+    
+    protected $table = "company_saferweb";
+
+    protected $fillable = [
+        'user_id',
+        'company_id',
+        'dot_number',
+        'mc_number',
+        'legal_name',
+        'dba_name',
+        'entity_type',
+        'physical_address',
+        'mailing_address',
+        'latest_update'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(UserCompany::class, 'company_id');
+    }
+
+}
