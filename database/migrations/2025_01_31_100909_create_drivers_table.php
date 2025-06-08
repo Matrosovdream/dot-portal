d7348bf41784500d91622982bff2faad45e4c734
Stan Matrosov <matrosovdream@gmail.com>
2025-06-08 16:48:22 +0200

    Drivers migration, new field status_id

--- database/migrations/2025_01_31_100909_create_drivers_table.php
+++ database/migrations/2025_01_31_100909_create_drivers_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('ssn')->nullable();
             $table->date('hire_date')->nullable();
             $table->foreignId('driver_type_id')->on('ref_driver_type');
+            $table->integer('status_id')->default(1)->nullable();
             $table->timestamps();
         });
 
