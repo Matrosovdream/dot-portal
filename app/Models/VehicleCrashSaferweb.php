476e3293fda218b10586792f8904089919779db5
Stan Matrosov <matrosovdream@gmail.com>
2025-06-09 15:05:44 +0200

    new model VehicleCrashSaferweb

---
+++ app/Models/VehicleCrashSaferweb.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VehicleCrashSaferweb extends Model
+{
+    
+    protected $table = "vehicle_crashes_saferweb";
+
+    protected $fillable = [
+        'vehicle_id',
+        'report_date',
+        'report_number',
+        'report_sequence_number',
+        'report_state',
+        'report_state_id',
+        'total_injuries',
+        'total_fatalities',
+        'api_data'
+    ];
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function reportState()
+    {
+        return $this->belongsTo(RefCountryStates::class, 'report_state_id');
+    }
+
+}
