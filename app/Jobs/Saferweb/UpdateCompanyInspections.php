c3ab04b055d8aa3c2fdc3df2da6a6998eaa67be3
Stan Matrosov <matrosovdream@gmail.com>
2025-06-09 16:21:47 +0200

    new job class UpdateCompanyInspections - blank version

---
+++ app/Jobs/Saferweb/UpdateCompanyInspections.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs\Saferweb;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Helpers\UserTaskHelper;
+use Log;
+
+class UpdateCompanyInspections implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public int $tries = 3;
+    public int $timeout = 5;
+
+    protected int $userId;
+
+    public function __construct(int $userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function handle(UserTaskHelper $taskHelper): void
+    {
+
+        Log::info("Updating user {$this->userId} from Saferweb API");
+     
+        /*
+        $taskHelper->updateCompanySnapshot(
+            $this->userId,
+        );
+        */
+
+    }
+
+}
