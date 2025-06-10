5f0969acb7ef1f6e327cf7a03537c722eb2febf0
Stan Matrosov <matrosovdream@gmail.com>
2025-06-10 10:23:18 +0200

    New class CompanyHelper, blank method updateSnapshot()

---
+++ app/Helpers/User/CompanyHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Helpers\User;
+
+use App\Mixins\Integrations\SaferwebAPI;
+use App\Repositories\User\UserRepo;
+
+class CompanyHelper {
+
+    public function updateCompanySnapshot(int $user_id): array|null
+    {
+
+        $apiService = app(SaferwebAPI::class);
+        $userRepo = app(UserRepo::class);
+
+        $user = $userRepo->getByID($user_id);
+        $dotNumber = $user['company']['dot_number'] ?? null;
+
+        if ($dotNumber) {
+            $apiData = $apiService->getCompanySnapshot($dotNumber);
+        } else { return null; }
+
+        if ( 
+            empty($apiData['error']) &&
+            $apiData != null
+            ) { 
+            $user['Model']->company->update([
+                'mc_number' => $apiData['phone'],
+            ]);
+
+            return $apiData;
+
+        } else {
+            // Handle error or empty response
+            return null;
+        }
+    }
+
+}
