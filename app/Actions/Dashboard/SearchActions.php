bb2570e440a08ccd6b604598d01cdfd17b1251fd
Stan Matrosov <matrosovdream@gmail.com>
2025-06-12 12:45:45 +0200

    SearchActions, globalSearchAjax() - mapped models with the modelSearch() results

--- app/Actions/Dashboard/SearchActions.php
+++ app/Actions/Dashboard/SearchActions.php
@@ -1,26 +1,49 @@
 <?php
 namespace App\Actions\Dashboard;
 
-use App\Mixins\Integrations\SaferwebAPI;
-use App\Models\Service;
-use App\Repositories\User\UserTaskRepo;
-use App\Helpers\User\CompanyHelper;
-
+use App\Repositories\Vehicle\VehicleRepo;
+use App\Repositories\File\FileRepo;
+use App\Repositories\Driver\DriverRepo;
 
 
 class SearchActions {
 
-    private $todoRepo;
+    private $vehicleRepo;
+    private $fileRepo;
+    private $driverRepo;
 
     public function __construct()
     {
-        $this->todoRepo = new UserTaskRepo();
+        $this->vehicleRepo = new VehicleRepo();
+        $this->fileRepo = new FileRepo();
+        $this->driverRepo = new DriverRepo();
 
     }
 
     public function globalSearchAjax( $request )
     {
-        
+        $results = [];
+
+        $models = [
+            'vehicles' => ['repo' => $this->vehicleRepo, 'url' => 'dashboard.vehicles.index'],
+            'documents' => ['repo' => $this->fileRepo, 'url' => 'dashboard.documents.index'],
+            'drivers' => ['repo' => $this->driverRepo, 'url' => 'dashboard.drivers.index'],
+        ];
+
+        foreach ($models as $key => $modelData) {
+            $res = $modelData['repo']->modelSearch($request['q'], true, 25) ?? [];
+            $count = $res['Model']->count();
+
+            // We don't need model here
+            unset($res['Model']);
+
+            $results[$key] = $res;
+            $results[$key]['count'] = $count;
+            $results[$key]['url'] = route($modelData['url'], ['q' => $request['q']]);
+        }
+
+        return $results;
+
     }
 
 }
