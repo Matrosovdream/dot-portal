9370671a2959b313aa8f1c424d691b7610b10472
Stan Matrosov <matrosovdream@gmail.com>
2025-06-14 11:09:59 +0200

    VehicleObserver, blank version

---
+++ app/Observers/VehicleObserver.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+
+class VehicleObserver
+{
+
+    public function __construct()
+    {
+
+    }
+
+
+    public function created(User $user): void
+    {
+
+        
+    }
+
+
+    public function updated(User $user): void
+    {
+        //
+    }
+
+
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
