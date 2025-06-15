620d101cc97a105c7cf01efa86e79ff6e337f547
Stan Matrosov <matrosovdream@gmail.com>
2025-06-15 23:11:48 +0200

    new repo DriverMvrRepo

---
+++ app/Repositories/Driver/DriverMvrRepo.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Repositories\Vehicle;
+
+use App\Repositories\AbstractRepo;
+use App\Models\DriverMvr;
+use App\Repositories\File\FileRepo;
+
+
+class DriverMvrRepo extends AbstractRepo
+{
+
+    protected $model;
+
+    protected $fileRepo;
+
+    protected $fields = [];
+
+    public function __construct()
+    {
+        $this->model = new DriverMvr();
+
+        $this->fileRepo = new FileRepo();
+    }
+
+    public function mapItem($item)
+    {
+
+        if( empty($item) ) return null;
+
+        $res = [
+            'id' => $item->id,
+            'driver_id' => $item->driver_id,
+            'mvr_number' => $item->mvr_number,
+            'mvr_date' => $item->mvr_date,
+            'file_id' => $item->file_id,
+            'file' => $this->fileRepo->mapItem($item->file),
+            'Model' => $item
+
+        ];
+        return $res;
+    }
+
+}
