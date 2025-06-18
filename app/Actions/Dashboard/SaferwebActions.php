c0922e03448e02443c6f1f6deb52b5b6209a04e0
Stan Matrosov <matrosovdream@gmail.com>
2025-06-18 16:56:08 +0200

    SaferwebActions, crashes() - injected modelSearch()

--- app/Actions/Dashboard/SaferwebActions.php
+++ app/Actions/Dashboard/SaferwebActions.php
@@ -45,11 +45,22 @@ class SaferwebActions {
     public function crashes( $request )
     {
 
-        $items = $this->crashesRepo->getAll(
-            [
-                'company_id' => $request->user()->company->id ?? null,
-            ]
-        );
+        $filter = [];
+
+        // Filter by search form
+        $filter = ['company_id' => auth()->user()->company->id];
+
+        // Filter by search form
+        if( request()->has('q') && !empty(request()->input('q')) ) {
+
+            $items = $this->crashesRepo->modelSearch( request()->input('q'), false);
+            $item_ids = $items->pluck('id')->toArray();
+
+            $filter['id'] = $item_ids; 
+            
+        }
+
+        $items = $this->crashesRepo->getAll($filter, $paginate = 30);
 
         return [
             'title' => 'Crashes',
