576427e89bf36c47b3b8baea839723035e046d5e
Stan Matrosov <matrosovdream@gmail.com>
2025-06-19 10:09:02 +0200

    Company dashboard page, new elements from the settings

--- resources/views/dashboard/home/user.blade.php
+++ resources/views/dashboard/home/user.blade.php
@@ -19,8 +19,8 @@
                     <div class="card-header pt-5">
 
                         <h3 class="card-title align-items-start flex-column">
-                            <span class="card-label fw-bold text-gray-900">FMCSA Portal Credentials</span>
-                            <span class="text-gray-500 mt-1 fw-semibold fs-6">Your credentials here</span>
+                            <span class="card-label fw-bold text-gray-900">Company information</span>
+                            <!--<span class="text-gray-500 mt-1 fw-semibold fs-6">Your credentials here</span>-->
                         </h3>
 
                         <div class="card-toolbar">
@@ -31,24 +31,51 @@
                     <div class="card-body pt-5">
 
                         <div class="d-flex flex-stack">
-                            <div class="text-gray-700 fw-semibold fs-6 me-2">Status</div>
+                            <div class="text-gray-700 fw-semibold fs-6 me-2">USDOT</div>
                             <div class="d-flex align-items-senter">
-                                <span class="text-gray-900 fw-bolder fs-6">Invalid</span>
+                                <span class="text-gray-900 fw-bolder fs-6">
+                                    #{{ $user['company']['dot_number'] ?? '-' }}
+                                </span>
                             </div>
                         </div>
                         <div class="separator separator-dashed my-3"></div>
 
                         <div class="d-flex flex-stack">
-                            <div class="text-gray-700 fw-semibold fs-6 me-2">Last confirmed on</div>
+                            <div class="text-gray-700 fw-semibold fs-6 me-2">MC Number</div>
                             <div class="d-flex align-items-senter">
-                                <span class="text-gray-900 fw-bolder fs-6">02/03/2025</span>
+                                <span class="text-gray-900 fw-bolder fs-6">
+                                    #{{ $user['company']['mc_number'] ?? '-' }}
+                                </span>
                             </div>
                         </div>
                         <div class="separator separator-dashed my-3"></div>
+
+                        <div class="d-flex flex-stack">
+                            <div class="text-gray-700 fw-semibold fs-6 me-2">Last MCS-150 date</div>
+                            <div class="d-flex align-items-senter">
+                                <span class="text-gray-900 fw-bolder fs-6">
+                                    -
+                                </span>
+                            </div>
+                        </div>
+                        <div class="separator separator-dashed my-3"></div>
+
+                        <div class="d-flex flex-stack">
+                            <div class="text-gray-700 fw-semibold fs-6 me-2">ISS Score</div>
+                            <div class="d-flex align-items-senter">
+                                <span class="text-gray-900 fw-bolder fs-6">
+                                    -
+                                </span>
+                            </div>
+                        </div>
+                        <div class="separator separator-dashed my-3"></div>
+
 
                         <div class="d-flex flex-stack">
                             <div class="text-gray-700 fw-semibold fs-6 me-2">
-                                <a href="#" class="text-primary opacity-75-hover fs-6 fw-semibold">Update credentials</a>
+                                <a href="{{ route('dashboard.profile.company.edit') }}" class="text-primary opacity-75-hover fs-6 fw-semibold">
+                                    Update credentials
+                                </a>
                             </div>
                             <div class="d-flex align-items-senter">
 
