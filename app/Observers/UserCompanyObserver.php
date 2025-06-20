a922e441e288a8dd6f884df0d44ff65e23c1c11b
Stan Matrosov <matrosovdream@gmail.com>
2025-06-20 15:10:38 +0200

    UserCompanyObserver, initial version

---
+++ app/Observers/UserCompanyObserver.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Observers;
+
+
+use App\Models\UserCompany;
+use Artisan;
+
+class UserCompanyObserver
+{
+    public function __construct()
+    {
+
+    }
+
+    public function created(UserCompany $company): void
+    {
+        $this->updateSaferweb( $company );
+    }
+
+    public function updated(UserCompany $company): void
+    {
+        $this->updateSaferweb( $company );
+    }
+
+    public function deleted(UserCompany $company): void
+    {
+        
+    }
+
+    public function restored(UserCompany $company): void
+    {
+        
+    }
+
+    public function forceDeleted(UserCompany $company): void
+    {
+        //
+    }
+
+    private function updateSaferweb( $company ) {
+
+        Artisan::call('saferweb:update', ['user_id' => $company->user_id]);
+
+    }
+
+}
