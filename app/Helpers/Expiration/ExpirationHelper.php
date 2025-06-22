4be0b8e5e087f7ab69486e7469abf6dca92f8fe2
Stan Matrosov <matrosovdream@gmail.com>
2025-06-22 19:52:38 +0200

    ExpirationHelper, mappedModels() - new field Entity

--- app/Helpers/Expiration/ExpirationHelper.php
+++ app/Helpers/Expiration/ExpirationHelper.php
@@ -7,7 +7,6 @@ class ExpirationHelper
 
     public function getExpiredItems() {
 
-        $expiredItems = [];
         $currentDate = now();
         $models = $this->mappedModels();
 
@@ -17,14 +16,16 @@ class ExpirationHelper
             $margin = $modelData['margin'];
 
             // Fetch items that are expired or will expire within the margin
-            $items = $modelClass::where($field, '<=', $currentDate->addDays($margin))->pluck('id')->toArray();
+            $items = $modelClass::where($field, '<=', $currentDate->addDays($margin))->get()->toArray();
 
             if ( count($items) > 0 ) {
                 $models[$key]['items'] = $items;
+            } else {
+                unset($models[$key]); // Remove models with no expired items
             }
         }
 
-        return $expiredItems;
+        return $models;
 
     }
     
@@ -35,30 +36,35 @@ class ExpirationHelper
                 'title' => 'Driver license',
                 'field' => 'expiration_date',
                 'model' => 'App\Models\DriverLicense',
+                'entity' => 'driver',
                 'margin' => 30, // in days
             ],
             'driver_medical_card' => [
                 'title' => 'Driver medical card',
                 'field' => 'expiration_date',
                 'model' => 'App\Models\DriverMedicalCard',
+                'entity' => 'driver',
                 'margin' => 30, // in days
             ],
             'vehicle_reg_expire_date' => [
                 'title' => 'Vehicle registration expiration date',
                 'field' => 'reg_expire_date',
                 'model' => 'App\Models\Vehicle',
+                'entity' => 'vehicle',
                 'margin' => 30, // in days
             ],
             'vehicle_inspection_expire_date' => [
                 'title' => 'Vehicle inspection expiration date',
                 'field' => 'inspection_expire_date',
                 'model' => 'App\Models\Vehicle',
+                'entity' => 'vehicle',
                 'margin' => 30, // in days
             ],
             'insurance_vehicle' => [
                 'title' => 'Vehicle insurance',
                 'field' => 'end_date',
                 'model' => 'App\Models\InsuranceVehicle',
+                'entity' => 'vehicle',
                 'margin' => 30, // in days
             ],
         ];
