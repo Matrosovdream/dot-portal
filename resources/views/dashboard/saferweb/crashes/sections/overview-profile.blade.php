7a07a67b441f0d8e7f4cfe51c773cf2ab32958a2
Stan Matrosov <matrosovdream@gmail.com>
2025-06-25 12:17:11 +0200

    Crashes show page, layout for the Overview profile section

--- resources/views/dashboard/saferweb/crashes/sections/overview-profile.blade.php
+++ resources/views/dashboard/saferweb/crashes/sections/overview-profile.blade.php
@@ -1,71 +1,55 @@
+@php
+    
+    $fields = [
+        [
+            'label' => 'Unit VIN',
+            'value' => $data['unit_vin'] ?? '-',
+        ],
+        [
+            'label' => 'Report date',
+            'value' => dateFormat($data['report_date']) ?? '-',
+        ],
+        [
+            'label' => 'Report number',
+            'value' => $data['report_number'] ?? '-',
+        ],
+        [
+            'label' => 'Report sequence number',
+            'value' => $data['report_sequence_number'] ?? '-',
+        ],
+        [
+            'label' => 'Report state',
+            'value' => $data['report_state'] ?? '-',
+        ],
+    ];
+
+@endphp
+
 <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
     <div class="card-header cursor-pointer">
 
         <div class="card-title m-0">
-            <h3 class="fw-bold m-0">Profile Details</h3>
+            <h3 class="fw-bold m-0">Main information</h3>
         </div>
-
-        <a href="{{ route('dashboard.drivers.show.profile', $driver['id']) }}" class="btn btn-sm btn-primary align-self-center">Edit Profile</a>
 
     </div>
 
     <div class="card-body p-9">
 
-        <div class="row mb-7">
-            <label class="col-lg-4 fw-semibold text-muted">Full Name</label>
-            <div class="col-lg-8">
-                <span class="fw-bold fs-6 text-gray-800">
-                    {{ $driver['user']['firstname'] ?? '' }} {{ $driver['user']['lastname'] ?? '' }}
-                </span>
+        @foreach( $fields as $field )
+
+            <div class="row mb-7">
+                <label class="col-lg-4 fw-semibold text-muted">
+                    {{ $field['label'] }}
+                </label>
+                <div class="col-lg-8">
+                    <span class="fw-bold fs-6 text-gray-800">
+                        {{ $field['value'] }}
+                    </span>
+                </div>
             </div>
-        </div>
 
-        <div class="row mb-7">
-            <label class="col-lg-4 fw-semibold text-muted">Phone</label>
-            <div class="col-lg-8">
-                <span class="fw-bold fs-6 text-gray-800">
-                    {{ $driver['user']['phone'] ?? '-' }}
-                </span>
-            </div>
-        </div>
-
-        <div class="row mb-7">
-            <label class="col-lg-4 fw-semibold text-muted">Email</label>
-            <div class="col-lg-8">
-                <span class="fw-bold fs-6 text-gray-800">
-                    {{ $driver['user']['email'] ?? '-' }}
-                </span>
-            </div>
-        </div>
-
-        <div class="row mb-7">
-            <label class="col-lg-4 fw-semibold text-muted">DOB</label>
-            <div class="col-lg-8">
-                <span class="fw-bold fs-6 text-gray-800">
-                    {{ dateFormat( $driver['dob'] ?? null ) ?? '-' }}
-                </span>
-            </div>
-        </div>
-
-        <div class="row mb-7">
-            <label class="col-lg-4 fw-semibold text-muted">SSN</label>
-            <div class="col-lg-8">
-                <span class="fw-bold fs-6 text-gray-800">
-                    {{ $driver['ssn'] ?? '-' }}
-                </span>
-            </div>
-        </div>
-
-        @if( isset( $validation['errors']['general'] ) )
-
-            @include('dashboard.includes.alerts.default', [
-                'title' => 'Important Notice',
-                'text' => 'Please make sure that your profile is fully completed',
-                'link' => '',
-                'link_url' => '',
-            ])
-
-        @endif
+        @endforeach
 
     </div>
 </div>
