0569581e960cec9b4362d886f08315e8cf27fb61
Stan Matrosov <matrosovdream@gmail.com>
2025-06-25 14:35:59 +0200

    HomeDriverActions, injected userRepo

--- app/Actions/Dashboard/HomeDriverActions.php
+++ app/Actions/Dashboard/HomeDriverActions.php
@@ -2,18 +2,22 @@
 namespace App\Actions\Dashboard;
 
 use App\Models\Service;
+use App\Repositories\User\UserRepo;
 
 class HomeDriverActions {
 
+    private $userRepo;
+
     public function __construct()
-    {
-        // 
+    { 
+        $this->userRepo = new UserRepo;
     }
 
     public function index()
     {
         $data = [
-            'title' => 'Services list',
+            'title' => 'My dashboard',
+            'user' => $this->userRepo->getById(auth()->user()->id),
             'services' => Service::paginate(10)
         ];
 
