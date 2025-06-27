7ad0979fa4774e2b69402e5f2cd102871fff318b
Stan Matrosov <matrosovdream@gmail.com>
2025-06-27 10:50:33 +0200

    Todo index page, new columns - link, tab

--- resources/views/dashboard/todo/index.blade.php
+++ resources/views/dashboard/todo/index.blade.php
@@ -19,7 +19,9 @@
                         <tr class="text-start text-gray-500 fw-bold fs-7 text-uppercase gs-0">
                                 <th class="">Title</th>
                                 <th>Category</th>
+                                <th>Tab</th>
                                 <th class=" text-center">Added</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody class="fw-semibold text-gray-600">
@@ -33,12 +35,25 @@
                                     <td>
                                         @if(isset($task['category']))
                                             {{ ucfirst( $task['category'] ) }}
+
                                         @else
                                             <span class="text-muted">No category</span>
                                         @endif
                                     </td>
+                                    <td>
+                                        @if(isset($task['subcategory']))
+                                            {{ ucfirst( $task['subcategory'] ) }}
+                                        @else
+                                            <span class="text-muted">No tab</span>
+                                        @endif
+                                    </td>
                                     <td class="text-center">
                                         {{ dateFormat( $task['Model']->created_at ) }}
+                                    </td>
+                                    <td>
+                                        <a href="{{ $task['link'] }}" class="btn btn-sm btn-light-primary">
+                                            <i class="fa fa-eye"></i> View
+                                        </a>
                                     </td>
                                 </tr>
 
