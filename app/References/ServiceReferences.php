d7caf83e1972ecb48867a7c9be9cd45db0e88493
Stan Matrosov <matrosovdream@gmail.com>
2025-06-29 14:41:32 +0200

    ServiceReferences, changed the set of Predefined forms

--- app/References/ServiceReferences.php
+++ app/References/ServiceReferences.php
@@ -3,6 +3,47 @@ namespace App\References;
 
 class ServiceReferences {
 
+    public function getPredefinedForms() {
+
+        return [
+            1 => [
+                'id' => 1,
+                'name' => 'MCS-150',
+                'path' => 'forms.request.mcs150',
+                'fields' => []
+            ],
+            2 => [
+                'id' => 2,
+                'name' => 'UCR',
+                'path' => 'forms.request.ucr',
+                'fields' => [
+                    'range_of_units' => ['title' => 'Range of units', 'type' => 'text', 'required' => true],
+                    'date_of_birth' => ['title' => 'Date of birth', 'type' => 'date', 'required' => true],
+                    'permit_year' => ['title' => 'Permit year', 'type' => 'text', 'required' => true],
+                ]
+            ],
+            3 => [
+                'id' => 3,
+                'name' => 'Road Taxes',
+                'path' => 'forms.request.road-taxes',
+                'fields' => []
+            ],
+            4 => [
+                'id' => 4,
+                'name' => 'IFTA',
+                'path' => 'forms.request.ifta',
+                'fields' => []
+            ],
+            5 => [
+                'id' => 5,
+                'name' => 'IRP',
+                'path' => 'forms.request.irp',
+                'fields' => []
+            ],
+        ];
+        
+    }
+
     public static function serviceFields() {
 
         return [
@@ -64,81 +105,6 @@ class ServiceReferences {
             ],
         ];
 
-    }
-
-    public function getPredefinedForms() {
-
-        return [
-            1 => [
-                'id' => 1,
-                'name' => 'UCR',
-                'path' => 'forms.request.ucr',
-                'fields' => [
-                    'range_of_units' => ['title' => 'Range of units', 'type' => 'text', 'required' => true],
-                    'date_of_birth' => ['title' => 'Date of birth', 'type' => 'date', 'required' => true],
-                    'permit_year' => ['title' => 'Permit year', 'type' => 'text', 'required' => true],
-                ]
-            ],
-            2 => [
-                'id'=> 2,
-                'name' => 'Road Taxes'
-            ],
-            3 => [
-                'id'=> 3,
-                'name' => 'Annual report'
-            ],
-            4 => [
-                'id'=> 4,
-                'name' => 'IFTA'
-            ],
-            5 => [
-                'id'=> 5,
-                'name' => 'State permits'
-            ],
-            6 => [
-                'id'=> 6,
-                'name' => 'IRP Service'
-            ],
-            7 => [
-                'id'=> 7,
-                'name' => 'DOT Update'
-            ],
-            8 => [
-                'id'=> 8,
-                'name' => 'Fuel Tax Quarterly Filing'
-            ],
-            9 => [
-                'id'=> 9,
-                'name' => 'MVR',
-                'path' => 'forms.request.mvr',
-                'fields' => [
-                    'driver_id' => ['title' => 'Driver', 'type' => 'select', 'required' => true],
-                    'mvr_type' => ['title' => 'MVR type', 'type' => 'select', 'required' => true],
-                    'auth_document' => ['title' => 'Upload signed MVR authorization', 'type' => 'file', 'required' => true],
-                ]
-            ],
-            10 => [
-                'id'=> 10,
-                'name' => 'New Driver Setup'
-            ],
-            11 => [
-                'id'=> 11,
-                'name' => 'Add to Fleet'
-            ],
-            12 => [
-                'id'=> 12,
-                'name' => 'Equipment Inspection'
-            ],
-            13 => [
-                'id'=> 13,
-                'name' => 'Terminate Driver'
-            ],
-            14 => [
-                'id'=> 14,
-                'name' => 'Terminate Equipment'
-            ],
-        ];
-        
     }
 
     public function getFormFields($form_id=null) {
