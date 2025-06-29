931d6bc17cb923dce5cb7823c7c301f29acbf9a1
Stan Matrosov <matrosovdream@gmail.com>
2025-06-29 21:28:31 +0200

    mscForm class, new methods getFormData() and getReferences()

--- app/Helpers/RequestForms/Predefined/Forms/mscForm.php
+++ app/Helpers/RequestForms/Predefined/Forms/mscForm.php
@@ -24,6 +24,65 @@ class MscForm extends AbstractForm
         
     }
 
+    /**
+     * Get the form data.
+     *
+     * @return array
+     */
+    public function getFormData()
+    {
+        return [
+            'formName' => $this->formName,
+            'formTitle' => $this->formTitle,
+            'formDescription' => $this->formDescription,
+            'formFields' => $this->formFields,
+        ];
+    }
+
+    /**
+     * Get the references for the form fields.
+     *
+     * @return array
+     */
+    public function getReferences() {
+
+        $fields = [
+            'change_type' => [
+                'type' => 'select',
+                'label' => 'Change Type',
+                'options' => [
+                    ['value' => 'keep', 'title' => 'Keep same'],
+                    ['value' => 'change', 'title' => 'Change information'],
+                ],
+            ],
+            'operation_type' => [
+                'type' => 'select',
+                'label' => 'Operation Type',
+                'options' => [
+                    ['value' => 'interstate', 'title' => 'Interstate'],
+                    ['value' => 'intrastate', 'title' => 'Intrastate'],
+                ],
+            ],
+            'cargo_type' => [
+                'type' => 'select',
+                'label' => 'Cargo Type',
+                'options' => [
+                    ['value' => 'general', 'title' => 'General Freight'],
+                    ['value' => 'hazardous', 'title' => 'Hazardous Materials'],
+                    ['value' => 'household', 'title' => 'Household Goods'],
+                    ['value' => 'passenger', 'title' => 'Passenger'],
+                ],
+            ],
+            'country_state' => [
+                'type' => 'select',
+                'label' => 'State',
+                'options' => $this->getCountryStates(),
+            ],
+        ];
+
+        return $fields;
+
+    }
 
 
 }
