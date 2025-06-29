9dbb6052722bef3476f62985806ca88112e5680a
Stan Matrosov <matrosovdream@gmail.com>
2025-06-29 21:26:13 +0200

    New class AbstractForm (RequestForms)

---
+++ app/Helpers/RequestForms/Predefined/AbstractForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Helpers\RequestForms\Predefined;
+
+use App\Repositories\References\RefCountryStateRepo;
+
+class AbstractForm
+{
+    
+
+    public function __construct()
+    {
+        
+    }
+
+
+
+}
