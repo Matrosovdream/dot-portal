25210e3597dab4ab4fe2dbb83a52bf47c84f8d8b
Stan Matrosov <matrosovdream@gmail.com>
2025-06-29 21:05:37 +0200

    Component Dashboard Select, new blade template for the Inline template type

---
+++ resources/views/components/dashboard/forms/select-inline.blade.php
@@ -0,0 +1,37 @@
+@if($label)
+    <label 
+        class="col-lg-4 col-form-label fw-semibold fs-6 {{ $required ? 'required' : '' }}"
+        for="{{ $inputId ?? $inputName }}"
+        >
+        {{ $label }}
+    </label>
+@endif
+
+
+<div class="col-lg-4 fv-row">
+    <select
+        class="form-select form-select-lg form-select-solid"
+        data-control="select2"
+        data-hide-search="true"
+        data-placeholder="{{ $label }}"
+        name="{{ $inputName }}{{ $multiple ? '[]' : '' }}"
+        id="{{ $inputId ?? $inputName }}"
+        {{ $required ? 'required' : '' }}
+        {{ $multiple ? 'multiple' : '' }}
+    >
+    <option selected disabled></option>
+        @foreach($options as $option)
+            <option value="{{ $option['value'] }}" @if($value == $option['value']) selected @endif>
+                {{ $option['title'] }}
+            </option>
+        @endforeach
+    </select>
+</div>
+
+@if($note)
+    <div class="form-text">{{ $note }}</div>
+@endif
+
+@if($description)
+    <div class="text-muted fs-7">{{ $description }}</div>
+@endif
