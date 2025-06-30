6e8315c9e08b53da1230fff576aa6034d718adf7
Stan Matrosov <matrosovdream@gmail.com>
2025-06-30 14:31:54 +0200

    Predefined form UCR, blade formatting

--- resources/views/forms/request/ucr.blade.php
+++ resources/views/forms/request/ucr.blade.php
@@ -1,23 +1,23 @@
 <div class="row mb-6">
     <x-select 
-                inputName="fields[range_units]"
-                label="Range of Units"
-                :options="$formRefs['range_units']['options']"
-                value="{{ $values['range_units'] ?? '' }}"
-                :multiple=false
-                :required=true
-                template="inline"
-            />
+        inputName="fields[range_units]"
+        label="Range of Units"
+        :options="$formRefs['range_units']['options']"
+        value="{{ $values['range_units'] ?? '' }}"
+        :multiple=false
+        :required=true
+        template="inline"
+    />
 </div>           
 
 <div class="row mb-6">
     <x-select 
-                inputName="fields[permit_year]"
-                label="Permit Year"
-                :options="$formRefs['permit_year']['options']"
-                value="{{ $values['permit_year'] ?? '' }}"
-                :multiple=false
-                :required=true
-                template="inline"
-            />
+        inputName="fields[permit_year]"
+        label="Permit Year"
+        :options="$formRefs['permit_year']['options']"
+        value="{{ $values['permit_year'] ?? '' }}"
+        :multiple=false
+        :required=true
+        template="inline"
+    />
 </div>
