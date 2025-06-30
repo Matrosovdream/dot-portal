5aeb6a7985dd02d424e2fb20a35d9b2775dece0e
Stan Matrosov <matrosovdream@gmail.com>
2025-06-30 14:27:48 +0200

    Predefined form UCR, added fields - range_units and permit_year

--- resources/views/forms/request/ucr.blade.php
+++ resources/views/forms/request/ucr.blade.php
@@ -1,44 +1,23 @@
-
-<div class="col-md-6 mb-3">
+<div class="row mb-6">
     <x-select 
-        inputName="fields[range_of_units]"
-        label="Select the range of units"
-        :options="[
-            ['value' => '0-2 Units', 'title' => '0-2 Units'],
-            ['value' => '3-5 Units', 'title' => '3-5 Units'],
-            ['value' => '6-20 Units', 'title' => '6-20 Units'],
-            ['value' => '21-100 Units', 'title' => '21-100 Units']
-        ]"
-        value="{{ $values['range_of_units'] ?? '' }}"
-        :multiple=false
-        :required=true
-    />
-</div>
+                inputName="fields[range_units]"
+                label="Range of Units"
+                :options="$formRefs['range_units']['options']"
+                value="{{ $values['range_units'] ?? '' }}"
+                :multiple=false
+                :required=true
+                template="inline"
+            />
+</div>           
 
-<div class="col-md-6 mb-3">
-    <x-date
-        inputName="fields[date_of_birth]"
-        label="Date of Birth"
-        :required=true
-        :placeholder="__('Enter your date of birth')"
-        :note="__('Please enter your date of birth in the format YYYY-MM-DD')"
-        :description="__('This field is required for age verification')"
-        value="{{ $values['date_of_birth'] ?? '' }}"
-    />
-</div>
-
-<div class="col-md-6 mb-3">
+<div class="row mb-6">
     <x-select 
-        inputName="fields[permit_year]"
-        label="Permit Year"
-        :options="[
-            ['value' => '2024', 'title' => '2024'],
-            ['value' => '2025', 'title' => '2025'],
-            ['value' => '2026', 'title' => '2026'],
-            ['value' => '2027', 'title' => '2027']
-        ]"
-        value="{{ $values['permit_year'] ?? '' }}"
-        :multiple=false
-        :required=true
-    />
+                inputName="fields[permit_year]"
+                label="Permit Year"
+                :options="$formRefs['permit_year']['options']"
+                value="{{ $values['permit_year'] ?? '' }}"
+                :multiple=false
+                :required=true
+                template="inline"
+            />
 </div>
