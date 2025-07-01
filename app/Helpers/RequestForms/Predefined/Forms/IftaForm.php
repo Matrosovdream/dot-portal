d0510e648e999a471be72055af0f3a98bfbf4baf
Stan Matrosov <matrosovdream@gmail.com>
2025-07-01 21:36:26 +0200

    Predefined IFTA form, injected validation and changed field names

--- app/Helpers/RequestForms/Predefined/Forms/IftaForm.php
+++ app/Helpers/RequestForms/Predefined/Forms/IftaForm.php
@@ -8,6 +8,56 @@ class IftaForm extends AbstractForm
 {
     protected $formName = 'ifta';
     protected $formTitle = 'IFTA';
+    protected $formFields = [
+        'request_type' => [
+            'type' => 'select',
+            'label' => 'Request Type',
+            'options' => [
+                'reg' => 'Registration',
+                'quarterly_filing' => 'Quarterly Filing',
+            ],
+            'required' => true,
+        ],
+        'country_state_id' => [
+            'type' => 'select',
+            'label' => 'State',
+            'required' => true,
+        ],
+        'vehicle_id' => [
+            'type' => 'select',
+            'label' => 'Vehicle',
+            'required' => true,
+        ],
+        'filing_period' => [
+            'type' => 'select',
+            'label' => 'Filing Period',
+        ],
+    ];
+
+
+    public function validateFormData($requestData)
+    {
+
+        $errors = [];
+
+        if( $requestData['request_type'] == 'reg' ) {
+            if( empty($requestData['country_state_id']) ) {
+                $errors['country_state'] = 'State is required field.';
+            }
+            if( empty($requestData['vehicle_id']) ) {
+                $errors['vehicle'] = 'Vehicle is required field.';
+            }
+        } elseif( $requestData['request_type'] == 'quarterly_filing' ) {
+            if( empty($requestData['filing_period']) ) {
+                $errors['filing_period'] = 'Period is required field.';
+            }
+            if( empty($requestData['vehicle_id']) ) {
+                $errors['vehicle'] = 'Vehicle is required field.';
+            }
+        }
+
+        return $errors;
+    }
 
     public function getReferences() {
 
