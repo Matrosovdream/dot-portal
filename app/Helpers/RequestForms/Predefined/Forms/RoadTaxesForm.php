e553d47d7cd611b2fd01d70b324524ee43bb3116
Stan Matrosov <matrosovdream@gmail.com>
2025-07-03 10:38:55 +0200

    Predefined form Road Taxes, new method prepareVehiclesFilter() and injected into getReferences()

--- app/Helpers/RequestForms/Predefined/Forms/RoadTaxesForm.php
+++ app/Helpers/RequestForms/Predefined/Forms/RoadTaxesForm.php
@@ -37,13 +37,26 @@ class RoadTaxesForm extends AbstractForm
             'vehicles' => [
                 'type' => 'select',
                 'label' => 'Vehicles',
-                'options' => $this->getVehicles(
-                    ['company_id' => auth()->user()->company->id ?? null]
-                ), 
+                'options' => $this->getVehicles( $this->prepareVehiclesFilter() ), 
             ],
         ];
 
         return $this->prepareReferences($fields);
+
+    }
+
+    private function prepareVehiclesFilter() {
+
+        $requestData = $this->requestData;
+
+        // Vehicle filter based on request data or user company
+        if( isset( $requestData['id'] ) ) {
+            $vehicleFilter = ['company_id' => $requestData['user']['company']['id']];
+        } else {
+            $vehicleFilter = ['company_id' => auth()->user()->company->id ?? null];
+        }
+
+        return $vehicleFilter;
 
     }
 
