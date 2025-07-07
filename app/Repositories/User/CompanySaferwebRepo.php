eacd8f4ecf3b857e452a028bebc3fe00bb7af5cd
Stan Matrosov <matrosovdream@gmail.com>
2025-07-07 16:29:10 +0200

    CompanySaferwebRepo, json_decode for the api_data

--- app/Repositories/User/CompanySaferwebRepo.php
+++ app/Repositories/User/CompanySaferwebRepo.php
@@ -57,7 +57,7 @@ class CompanySaferwebRepo extends AbstractRepo
             'physical_address' => $item->physical_address,
             'mailing_address' => $item->mailing_address,
             'latest_update' => $item->latest_update,
-            'api_data' => $item->api_data,
+            'api_data' => json_decode( $item->api_data, true ),
             'Model' => $item
         ];
 
