7351dad219ac6af9ba335537c6abe5475a471c1c
Stan Matrosov <matrosovdream@gmail.com>
2025-07-08 17:41:52 +0200

    Removed getItemsByDot() from TranspGov classes besides Abstract

--- app/Helpers/TranspGov/TranspGovSnapshot.php
+++ app/Helpers/TranspGov/TranspGovSnapshot.php
@@ -7,17 +7,4 @@ class TranspGovSnapshot extends TranspGovAbstract {
     protected $api;
     protected $apiRoute = 'company.snapshot';
 
-    public function getItemsByDot(array $usdots, int $paginate = 10000)
-    {
-
-        $filter = [];
-        $filter['dot_number'] = [
-            'value' => $usdots,
-            'operator' => 'IN'
-        ];
-
-        return $this->retrieveItems( $filter, $paginate, $groupBy = 'dot_number' );
-
-    } 
-
 }
