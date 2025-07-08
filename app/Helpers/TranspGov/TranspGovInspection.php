f8dad44b76c808748c86d85676425b3d3cbb6bc1
Stan Matrosov <matrosovdream@gmail.com>
2025-07-08 19:31:42 +0200

    TransportGov helpers, added/changed mapItemsForModel() methods

--- app/Helpers/TranspGov/TranspGovInspection.php
+++ app/Helpers/TranspGov/TranspGovInspection.php
@@ -7,4 +7,26 @@ class TranspGovInspection extends TranspGovAbstract {
     protected $api;
     protected $apiRoute = 'inspection.history';
 
+    public function mapItemsForModel(array $items) {
+
+        $itemsNew = [];
+        foreach ($items as $item) {
+            $itemNew = [
+                'unit_vin' => $item['vehicle_identification_number'] ?? null,   
+                'dot_number' => $item['dot_number'] ?? null,
+                'unique_id' => $item['inspection_id'] ?? null,
+                'report_date' => $item['insp_date'] ?? $this->parseDate($item['insp_date']) ?? null,
+                'report_number' => $item['report_number'] ?? null,
+                'inspection_level' => $item['insp_level_id'] ?? null,
+                'report_state' => $item['report_state'] ?? null,
+                'api_data' => $item
+            ];
+            
+            $itemsNew[] = $itemNew;
+        }
+
+        return $itemsNew;
+
+    }
+
 }
--- app/Helpers/TranspGov/TranspGovSnapshot.php
+++ app/Helpers/TranspGov/TranspGovSnapshot.php
@@ -7,4 +7,58 @@ class TranspGovSnapshot extends TranspGovAbstract {
     protected $api;
     protected $apiRoute = 'company.snapshot';
 
+    public function mapItemsForModel(array $items) {
+
+        $itemsNew = [];
+        foreach ($items as $item) {
+            $itemNew = [
+                'dot_number' => $item['dot_number'] ?? null,
+                'mc_number' => $item['mc_number'] ?? null,
+                'legal_name' => $item['legal_name'] ?? null,
+                'dba_name' => $item['dba_name'] ?? null,
+                'entity_type' => $item['entity_type'] ?? null,
+                'physical_address' => $this->makePhysicalAddress($item),
+                'mailing_address' => $this->makeMailingAddress($item),
+                'latest_update' => $item['mcs150_date'] ?? $this->parseDate($item['mcs150_date']) ?? null,
+                'api_data' => $item
+            ];
+            
+            $itemsNew[] = $itemNew;
+        }
+
+        dd($itemsNew);
+
+        return $itemsNew;
+
+    }
+
+    protected function makePhysicalAddress(array $item): string {
+        
+        $elements = ['phy_street', 'phy_city', 'phy_country', 'phy_state', 'phy_zip', 'phy_cnty'];
+        return $this->makeAddress($item, $elements);
+
+    }
+
+    protected function makeMailingAddress(array $item): string {
+        
+        $elements = ['carrier_mailing_street', 'carrier_mailing_city', 'carrier_mailing_country', 
+                     'carrier_mailing_state', 'carrier_mailing_zip', 'carrier_mailing_cnty'];
+        return $this->makeAddress($item, $elements);
+
+    }
+
+    protected function makeAddress(array $item, array $elements): string {
+        
+        $address = [];
+        foreach ($elements as $element) {
+            if (isset($item[$element]) && !empty($item[$element])) {
+                $address[] = $item[$element];   
+            }
+        }
+
+        return implode(', ', $address);
+
+    }
+
+
 }
