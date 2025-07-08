33c040cc891e9470b8c11ebc45422c202d232e05
Stan Matrosov <matrosovdream@gmail.com>
2025-07-08 19:53:58 +0200

    TransportGov helpers, fixed parseDate() implementation

--- app/Helpers/TranspGov/TranspGovInspection.php
+++ app/Helpers/TranspGov/TranspGovInspection.php
@@ -15,7 +15,7 @@ class TranspGovInspection extends TranspGovAbstract {
                 'unit_vin' => $item['vehicle_identification_number'] ?? null,   
                 'dot_number' => $item['dot_number'] ?? null,
                 'unique_id' => $item['inspection_id'] ?? null,
-                'report_date' => $item['insp_date'] ?? $this->parseDate($item['insp_date']) ?? null,
+                'report_date' => $this->parseDate( $item['insp_date'] ?? '' ),
                 'report_number' => $item['report_number'] ?? null,
                 'inspection_level' => $item['insp_level_id'] ?? null,
                 'report_state' => $item['report_state'] ?? null,
