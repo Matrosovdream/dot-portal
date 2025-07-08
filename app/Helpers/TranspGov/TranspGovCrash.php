7780dd24065917522754c49e92891f01f6a305fa
Stan Matrosov <matrosovdream@gmail.com>
2025-07-08 18:50:56 +0200

    TranspGovCrash, new method mapItemsForModel()

--- app/Helpers/TranspGov/TranspGovCrash.php
+++ app/Helpers/TranspGov/TranspGovCrash.php
@@ -7,4 +7,26 @@ class TranspGovCrash extends TranspGovAbstract {
     protected $api;
     protected $apiRoute = 'crash.history';
 
+    public function mapItemsForModel(array $items) {
+        
+        $itemsNew = [];
+        foreach ($items as $item) {
+            $itemNew = [
+                'unit_vin' => $item['vehicle_identification_number'] ?? null,   
+                'dot_number' => $item['dot_number'] ?? null,
+                'report_date' => $item['report_date'] ?? $this->parseDate($item['report_date']) ?? null,
+                'report_number' => $item['report_number'] ?? null,
+                'report_state' => $item['report_state'] ?? null,
+                'total_injuries' => $item['injuries'] ?? null,
+                'total_fatalities' => $item['fatalities'] ?? null,
+                'api_date' => $item
+            ];
+            
+            $itemsNew[] = $itemNew;
+        }
+
+        return $itemsNew;
+
+    }
+
 }
