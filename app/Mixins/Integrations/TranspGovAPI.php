c3d46575bc6fcc69506e2291b793223553bf1490
Stan Matrosov <matrosovdream@gmail.com>
2025-07-08 11:00:04 +0200

    TranspGovAPI, new method setFilter()

--- app/Mixins/Integrations/TranspGovAPI.php
+++ app/Mixins/Integrations/TranspGovAPI.php
@@ -6,10 +6,16 @@ class TranspGovAPI {
 
     protected $apiKey;
     protected $baseUrl = 'https://api.transpgov.com/v1/';
+    public $filter = [];
 
     public function __construct()
     {
         $this->apiKey = config('services.transpgov.api_key');
+    }
+
+    public function setFilter(array $filter): void
+    {
+        $this->filter = $filter;
     }
 
 }
