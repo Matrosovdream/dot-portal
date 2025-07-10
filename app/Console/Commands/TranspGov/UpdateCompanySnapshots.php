12b523e899fc773536bae568c7a61e37eb451f0f
Stan Matrosov <matrosovdream@gmail.com>
2025-07-10 16:02:03 +0200

    Command UpdateCompanySnapshots, added chunking feature

--- app/Console/Commands/TranspGov/UpdateCompanySnapshots.php
+++ app/Console/Commands/TranspGov/UpdateCompanySnapshots.php
@@ -8,9 +8,10 @@ use App\Jobs\TranspGov\UpdateCompanySnapshots as SnapshotJob;
 
 class UpdateCompanySnapshots extends Command {
 
-    protected $signature = 'transgov:update-snapshots';
+    protected $signature = 'transgov:update-snapshots {--chunk=50}';
 
     protected $description = 'Update Company Snapshots from the Transport Government API';
+    protected $delayChunks = 1; // Delay between chunks in seconds, can be adjusted as needed
     protected UserCompanyRepo $userCompanyRepo;
 
     public function __construct()
@@ -24,16 +25,26 @@ class UpdateCompanySnapshots extends Command {
     public function handle()
     {
 
-        $companies = $this->userCompanyRepo->getAll($filter=[], $paginate = 20, ['id' => 'asc']);
+        $chunkSize = (int)$this->option('chunk');
+
+        $companies = $this->userCompanyRepo->getAll($filter=[], $paginate = 100, ['id' => 'asc']);
         $company_ids = $companies['Model']->pluck('dot_number', 'id')->toArray();
 
-        $helper = app('App\Helpers\Company\CompanyIntegrationHelper');
-        $helper->updateSnapshots($company_ids);
+        // Chunk the company IDs to avoid memory issues
+        $company_ids = array_chunk($company_ids, $chunkSize, true);
 
-        // Company Snapshots
-        SnapshotJob::dispatch($company_ids);
+        foreach ($company_ids as $chunk) {
 
-        $this->info("Transport Government company IDs have been added to the queue.");
+            //$helper = app('App\Helpers\Company\CompanyIntegrationHelper');
+            //$helper->updateSnapshots($chunk);
+
+            // Dispatch the job for each chunk
+            SnapshotJob::dispatch($chunk)->delay(now()->addSeconds($this->delayChunks));
+
+            $this->info("Transport Government company IDs have been added to the queue: " . implode(', ', $chunk));
+        }
+
+        
     }
 
 }
