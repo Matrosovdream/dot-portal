9b124c8ebecf662629432545fb2ff39c08cd48ab
Stan Matrosov <matrosovdream@gmail.com>
2025-07-10 11:46:16 +0200

    UpdateCompanySnapshots, injected CompanyIntegrationHelper

--- app/Jobs/TranspGov/UpdateCompanySnapshots.php
+++ app/Jobs/TranspGov/UpdateCompanySnapshots.php
@@ -2,26 +2,44 @@
 
 namespace App\Jobs\TranspGov;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Helpers\Company\CompanyIntegrationHelper as IntegrationHelper;
+use Log;
 
 class UpdateCompanySnapshots implements ShouldQueue
 {
-    use Queueable;
 
-    /**
-     * Create a new job instance.
-     */
-    public function __construct()
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public int $tries = 3;
+    public int $timeout = 5;
+
+    public array $company_ids;
+
+    public function __construct(array $company_ids)
     {
-        //
+        $this->company_ids = $company_ids;
     }
 
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
+    
+    public function handle(IntegrationHelper $integrationHelper): void
     {
-        //
+
+        $companiesStr = implode(', ', $this->company_ids);
+
+        $this->log("Updating company snapshot for companies: { $companiesStr }");
+     
+        $res = $integrationHelper->updateSnapshots( $this->company_ids );
+
     }
+
+    private function log(string $message): void
+    {
+        Log::info($message);
+    }
+
 }
