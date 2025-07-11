d71f1b0954f0fab45db3f39c5fe9cd8f20b3b8a8
Stan Matrosov <matrosovdream@gmail.com>
2025-07-11 10:56:53 +0200

    Predefined MSC-150 form, new blade partial for the Hazardous materials

---
+++ resources/views/forms/request/partials/msc150/hazardous-materials.blade.php
@@ -0,0 +1,65 @@
+<div class="row mb-6">
+
+    <label class="col-lg-4 col-form-label fw-semibold fs-6 required" for="fields[cargo_type]">
+      Hazardous Materials
+    </label>
+      
+    <table class="table table-bordered">
+      <thead>
+          <tr>
+              <th>Title</th>
+              <th>Carrier</th>
+              <th>Shipper</th>
+              <th>Bulk</th>
+              <th>Non-Bulk</th>
+          </tr>
+      </thead>
+      <tbody>
+          @foreach($formRefs['hazardous_materials']['options'] as $item)
+              <tr>
+                    <td>
+                        {{ $item['title'] }}
+                    </td>
+                    <td class="clickable-cell text-center">
+                        <input 
+                            type="checkbox" 
+                            class="form-check-input form-check-input-lg" 
+                            name="{{ $item['value'] }}_carrier" 
+                            @if( isset( $values[ $item['value'].'_carrier' ] ) ) checked @endif
+                            value="1"
+                            >
+                    </td>
+                    <td class="clickable-cell text-center">
+                        <input 
+                            type="checkbox" 
+                            class="form-check-input form-check-input-lg" 
+                            name="{{ $item['value'] }}_shippen" 
+                            @if( isset( $values[ $item['value'].'_shipper' ] ) ) checked @endif
+                            value="1"
+                            >
+                    </td>
+                    <td class="clickable-cell text-center">
+                        <input 
+                            type="checkbox" 
+                            class="form-check-input form-check-input-lg" 
+                            name="{{ $item['value'] }}_bulk" 
+                            @if( isset( $values[ $item['value'].'_bulk' ] ) ) checked @endif
+                            value="1"
+                            >
+                    </td>
+                    <td class="clickable-cell text-center">
+                        <input 
+                            type="checkbox" 
+                            class="form-check-input form-check-input-lg" 
+                            name="{{ $item['value'] }}_non_bulk" 
+                            @if( isset( $values[ $item['value'].'_non_bulk' ] ) ) checked @endif
+                            value="1"
+                            >
+                    </td>
+              </tr>
+          @endforeach
+      </tbody>
+    </table>
+  
+    
+  </div>
