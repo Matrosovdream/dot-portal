f7f22864f8281838200930db5096572da5e43460
Stan Matrosov <matrosovdream@gmail.com>
2025-07-21 15:22:51 +0200

    RegisteredUserController, finished method retrieveUsdot()

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -80,12 +80,21 @@ class RegisteredUserController extends Controller
     {
         $usdot = $request->input('usdot');
 
+        $dotApi = app('App\Helpers\TranspGov\TranspGovSnapshot');
+
+        // Use the TranspGovSnapshot helper to retrieve USDOT information
+        $snapshot = $dotApi->getByDot($usdot);
+
+        if (!$snapshot) {
+            return response()->json([]);
+        }
+
         // to retrieve the USDOT information. For this example, we'll just return a dummy response.
         $response = [
             'usdot' => $usdot,
-            'company_name' => 'Example Company',
-            'trucks_number' => 5,
-            'drivers_number' => 10,
+            'company_name' => $snapshot['legal_name'] ?? '',
+            'trucks_number' => $snapshot['truck_units'] ?? 0,
+            'drivers_number' => $snapshot['total_drivers'] ?? 0,
         ];
 
         return response()->json($response);
