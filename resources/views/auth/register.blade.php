20c680532da57be59f6a649cf2733f49aee4e108
Stan Matrosov <matrosovdream@gmail.com>
2025-07-24 15:53:39 +0200

    New component RegisterLayout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,242 +1,122 @@
-<x-guest-layout>
-
-    <form class="form w-100" novalidate="novalidate" id="kt_sign_up_form" method="POST" action="{{ route('register') }}">
+<x-register-layout>
+    
+    <form class="form w-100" id="kt_sign_in_form"
+        method="POST" action="{{ route('login') }}">
 
         @csrf
 
         <div class="text-center mb-11">
-            <h1 class="text-gray-900 fw-bolder mb-3">Sign Up</h1>
+            <h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
             <div class="text-gray-500 fw-semibold fs-6"></div>
         </div>
 
-        <div class="fv-row mb-8">
-            <x-input-label for="name" :value="__('Official’s full name')" />
-            <x-text-input 
-                id="name" 
-                class="form-control bg-transparent {{ $errors->has('name') ? 'is-invalid' : '' }}" 
-                type="text" 
-                name="name" 
-                :value="old('name')" 
-                required autofocus autocomplete="name" 
-                />
+        <!--
+        <div class="row g-3 mb-9">
+            <div class="col-md-6">
+                <a href="#"
+                    class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+                    <img alt="Logo" src="{{ asset('assets/admin/media/svg/brand-logos/google-icon.svg') }}"
+                        class="h-15px me-3" />Sign in with Google</a>
+            </div>
+            <div class="col-md-6">
+                <a href="#"
+                    class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+                    <img alt="Logo" src="{{ asset('assets/admin/media/svg/brand-logos/apple-black.svg') }}"
+                        class="theme-light-show h-15px me-3" />
+                    <img alt="Logo" src="{{ asset('assets/admin/media/svg/brand-logos/apple-black-dark.svg') }}"
+                        class="theme-dark-show h-15px me-3" />Sign in with Apple</a>
+            </div>
         </div>
+        
+        <div class="separator separator-content my-14">
+            <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
+        </div>
+        -->
 
+        <!-- Email Address -->
         <div class="fv-row mb-8">
             <x-input-label for="email" :value="__('Email')" />
-           <x-text-input 
-            id="email" 
-            class="form-control bg-transparent {{ $errors->has('email') ? 'is-invalid' : '' }}" 
-            type="email" 
-            name="email" 
-            :value="old('email')" 
-            required autocomplete="username" 
-            />
+            <x-text-input id="email" class="form-control bg-transparent" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Password -->
         <div class="fv-row mb-8">
-            <x-input-label for="phone" :value="__('Phone number')" />
-           <x-text-input 
-            id="phone" 
-            class="form-control bg-transparent {{ $errors->has('phone') ? 'is-invalid' : '' }}" 
-            type="text" 
-            name="phone" 
-            :value="old('phone')" 
-            required 
-            />
+            <x-input-label for="password" :value="__('Password')" />
+            <x-text-input id="password" class="form-control bg-transparent" type="password" name="password" required
+                autocomplete="current-password" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="fv-row mb-8 position-relative">
-            <x-input-label for="usdot" :value="__('USDOT number')" />
-           <x-text-input 
-            id="usdot" 
-            class="form-control bg-transparent {{ $errors->has('usdot') ? 'is-invalid' : '' }}" 
-            type="text" 
-            name="usdot" 
-            :value="old('usdot')" 
-            required 
-            />
 
-            <span
-                id="usdot-loader"
-                class="spinner-border text-primary position-absolute top-50 end-0 d-none"
-                style="margin-top: -3px; margin-right: 8px;"
-                role="status"
-                >
-                <span class="visually-hidden">Loading...</span>
-            </span>
-
-        </div>
-
-        <div class="fv-row mb-8">
-            <x-input-label for="company_name" :value="__('Company name')" />
-            <x-text-input 
-                id="company_name" 
-                class="form-control bg-transparent {{ $errors->has('company_name') ? 'is-invalid' : '' }}" 
-                type="text" 
-                name="company_name" 
-                :value="old('company_name')" 
-                required 
-                />
-        </div>
-
-        <div class="fv-row mb-10">
-            <!--begin::Label-->
-            <x-input-label for="trucks_number" :value="__('Number of trucks')" />
-            <br/><br/>
-        
-            <!-- Hidden input to sync with slider -->
-            <x-text-input 
-                id="trucks_number" 
-                class="form-control bg-transparent {{ $errors->has('trucks_number') ? 'is-invalid' : '' }} d-none" 
-                type="number"
-                name="trucks_number" 
-                :value="old('trucks_number', 10)" 
-                required 
-            />
-        
-            <div class="d-flex flex-stack">
-                <div id="slider_trucks_number_value" class="fs-7 fw-semibold text-muted"></div>
-                <div id="slider_trucks_number_slider" class="noUi-sm w-100 ms-5 me-8"></div>
+        @if (Route::has('password.request'))
+            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                <div></div>
+                <a 
+                    href="{{ route('password.request') }}" 
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Forgot Password ?
+                </a>
             </div>
+        @endif
 
-        </div>
-
-        <!-- Number of drivers -->
-        <div class="fv-row mb-10">
-            <x-input-label for="drivers_number" :value="__('Number of drivers')" />
-            <br/><br/>
-
-            <x-text-input 
-                id="drivers_number" 
-                class="form-control bg-transparent {{ $errors->has('drivers_number') ? 'is-invalid' : '' }} d-none" 
-                type="number" 
-                name="drivers_number" 
-                :value="old('drivers_number', 10)" 
-                required 
-            />
-
-            <div class="d-flex flex-stack">
-                <div id="slider_drivers_number_value" class="fs-7 fw-semibold text-muted"></div>
-                <div id="slider_drivers_number_slider" class="noUi-sm w-100 ms-5 me-8"></div>
-            </div>
-        </div>  
-
-        <div class="fv-row mb-8">
-           <x-input-label for="password" :value="__('Password')" />
-
-           <x-text-input 
-            id="password" 
-            class="form-control bg-transparent {{ $errors->has('password') ? 'is-invalid' : '' }}"
-            type="password"
-            name="password"
-            required autocomplete="new-password" 
-            />
-        </div>
-
-        <div class="fv-row mb-8">
-           <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-           <x-text-input 
-            id="password_confirmation" 
-            class="form-control bg-transparent {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}"
-            type="password"
-            name="password_confirmation" 
-            required autocomplete="new-password" 
-            />
-
-        @php /*
-           <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        */ @endphp
-       </div>
-
-        <div class="fv-row mb-8">
-            <label class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" name="toc" value="1" />
-                <span class="form-check-label fw-semibold text-gray-700 fs-base ms-1">
-                    I Accept the
-                    <a href="#" class="ms-1 link-primary">Terms</a>
-                </span>
-            </label>
-        </div>
-        
         <div class="d-grid mb-10">
-            <x-primary-button class="btn btn-primary">
-               {{ __('Register') }}
-           </x-primary-button>
-        </div>
 
-        <div class="text-gray-500 text-center fw-semibold fs-6">Already have an Account?
-            <a href="{{ route('login') }}" class="link-primary fw-semibold">Sign in</a>
-        </div>
+            <x-primary-button id="kt_sign_in_submit" class="btn btn-primary">
+                {{ __('Log in') }}
+            </x-primary-button>
 
+        </div>
+        <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
+            <a href="{{ route('register') }}" class="link-primary">Sign up</a>
+        </div>
     </form>
 
-</x-guest-layout>
+    <?php /*
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
 
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
 
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
 
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
 
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
 
-@include('auth.partials.register-js')
-    
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+    */ ?>
 
-
-
-
-
-
-<?php /*
-<x-guest-layout>
-   <form method="POST" action="{{ route('register') }}">
-       @csrf
-
-       <!-- Name -->
-       <div>
-           <x-input-label for="name" :value="__('Name')" />
-           <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-           <x-input-error :messages="$errors->get('name')" class="mt-2" />
-       </div>
-
-       <!-- Email Address -->
-       <div class="mt-4">
-           <x-input-label for="email" :value="__('Email')" />
-           <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-           <x-input-error :messages="$errors->get('email')" class="mt-2" />
-       </div>
-
-       <!-- Password -->
-       <div class="mt-4">
-           <x-input-label for="password" :value="__('Password')" />
-
-           <x-text-input id="password" class="block mt-1 w-full"
-                           type="password"
-                           name="password"
-                           required autocomplete="new-password" />
-
-           <x-input-error :messages="$errors->get('password')" class="mt-2" />
-       </div>
-
-       <!-- Confirm Password -->
-       <div class="mt-4">
-           <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-           <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                           type="password"
-                           name="password_confirmation" required autocomplete="new-password" />
-
-           <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-       </div>
-
-       <div class="flex items-center justify-end mt-4">
-           <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-               {{ __('Already registered?') }}
-           </a>
-
-           <x-primary-button class="ms-4">
-               {{ __('Register') }}
-           </x-primary-button>
-       </div>
-   </form>
-</x-guest-layout>
+</x-register-layout>
