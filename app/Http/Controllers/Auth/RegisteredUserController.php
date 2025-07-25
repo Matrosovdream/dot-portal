7e3af22bd98bea00b45a4d7e7cae44eb6a030680
Stan Matrosov <matrosovdream@gmail.com>
2025-07-25 22:52:24 +0200

    RegisteredUserController, injected action method into retrieveUsdot()

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,19 +11,25 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use App\Actions\Auth\RegisterUserActions;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     */
+
+    protected $actions;
+
+    public function __construct()
+    {
+        $this->actions = app(RegisterUserActions::class);
+    }
+
     public function create(): View
     {
 
         $data = [
             'title' => 'Register',
             'description' => 'Create a new account to access the DOT Portal.',
-            'steps' => $this->getRegSteps(),
+            'steps' => $this->actions->getRegSteps(),
         ];
 
         return view('auth.register', $data);
@@ -85,64 +91,9 @@ class RegisteredUserController extends Controller
 
     public function retrieveUsdot(Request $request)
     {
-        $usdot = $request->input('usdot');
-
-        $dotApi = app('App\Helpers\TranspGov\TranspGovSnapshot');
-
-        // Use the TranspGovSnapshot helper to retrieve USDOT information
-        $snapshot = $dotApi->getByDot($usdot);
-
-        if (!$snapshot) {
-            return response()->json([]);
-        }
-
-        // to retrieve the USDOT information. For this example, we'll just return a dummy response.
-        $response = [
-            'usdot' => $usdot,
-            'company_name' => $snapshot['legal_name'] ?? '',
-            'trucks_number' => $snapshot['truck_units'] ?? 0,
-            'drivers_number' => $snapshot['total_drivers'] ?? 0,
-        ];
-
-        return response()->json($response);
-    }
-
-    public function getRegSteps(): array
-    {
-        $steps = [
-            'account' => [
-                'number' => '1',
-                'title' => 'Account Info',
-                'slug' => 'account',
-                'description' => 'Setup your account settings'
-            ],
-            'company' => [
-                'number' => '2',
-                'title' => 'Company Details',
-                'slug' => 'company',
-                'description' => 'Setup your Company details'
-            ],
-            'billing' => [
-                'number' => '3',
-                'title' => 'Billing Details',
-                'slug' => 'billing',
-                'description' => 'Provide your payment info'
-            ],
-            'payment' => [
-                'number' => '4',
-                'title' => 'Subscription payment',
-                'slug' => 'payment',
-                'description' => 'Payment for your subscription'
-            ]
-        ];
-    
-        // Set active based GET parameter 'step' and if empty then default to 'account'
-        $activeStep = request()->get('step', 'account');    
-        foreach( $steps as $key => $details ) {
-            $steps[$key]['active'] = ($details['slug'] == $activeStep);
-        }
-
-        return $steps;
+        return response()->json(
+            $this->actions->retrieveUsdot($request)
+        );
     }
 
 }
