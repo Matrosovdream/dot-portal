6931f94d2cca8c8d7c3a334af2d9ed1920df6433
Stan Matrosov <matrosovdream@gmail.com>
2025-07-25 19:39:38 +0200

    RegisteredUserController, getRegSteps() - changed the keys for the

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -19,7 +19,14 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+
+        $data = [
+            'title' => 'Register',
+            'description' => 'Create a new account to access the DOT Portal.',
+            'steps' => $this->getRegSteps(),
+        ];
+
+        return view('auth.register', $data);
     }
 
     /**
@@ -98,6 +105,44 @@ class RegisteredUserController extends Controller
         ];
 
         return response()->json($response);
+    }
+
+    public function getRegSteps(): array
+    {
+        $steps = [
+            'account' => [
+                'number' => '1',
+                'title' => 'Account Info',
+                'slug' => 'account',
+                'description' => 'Setup your account settings'
+            ],
+            'company' => [
+                'number' => '2',
+                'title' => 'Company Details',
+                'slug' => 'company',
+                'description' => 'Setup your Company details'
+            ],
+            'billing' => [
+                'number' => '3',
+                'title' => 'Billing Details',
+                'slug' => 'billing',
+                'description' => 'Provide your payment info'
+            ],
+            'payment' => [
+                'number' => '4',
+                'title' => 'Subscription payment',
+                'slug' => 'payment',
+                'description' => 'Payment for your subscription'
+            ]
+        ];
+    
+        // Set active based GET parameter 'step' and if empty then default to 'account'
+        $activeStep = request()->get('step', 'account');    
+        foreach( $steps as $key => $details ) {
+            $steps[$key]['active'] = ($details['slug'] == $activeStep);
+        }
+
+        return $steps;
     }
 
 }
