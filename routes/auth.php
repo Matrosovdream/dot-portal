ae4095d052265e6d773fb454d1297482f13fcb54
Stan Matrosov <matrosovdream@gmail.com>
2025-07-28 17:04:16 +0200

    Routes Auth, register routes are visible for all

--- routes/auth.php
+++ routes/auth.php
@@ -11,15 +11,15 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+// This pages are available for registration and user management
+Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('register', [RegisteredUserController::class, 'store']);
+
+// retrieve-usdot
+Route::post('retrieve-usdot', [RegisteredUserController::class, 'retrieveUsdot'])->name('retrieve-usdot'); 
+
+// This routes are available for guest users
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    // retrieve-usdot
-    Route::post('retrieve-usdot', [RegisteredUserController::class, 'retrieveUsdot'])
-        ->name('retrieve-usdot'); 
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
