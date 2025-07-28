978b6e89907f6f94ecd8726779ae853e8b403ebb
Stan Matrosov <matrosovdream@gmail.com>
2025-07-28 17:31:27 +0200

    User migration, new user field reg_step

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_active')->default(true);
+            $table->string('reg_step')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
