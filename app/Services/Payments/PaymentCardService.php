988e7d692979e861b706dad8ba16b5f2e265c669
Stan Matrosov <matrosovdream@gmail.com>
2025-08-01 21:13:00 +0200

    PaymentCardService, initial version

---
+++ app/Services/Payments/PaymentCardService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services\Payments;
+
+use App\Repositories\User\UserPaymentCardRepo;
+use App\Actions\Dashboard\SubscriptionUserActions;
+
+class PaymentCardService
+{
+    
+    public function storeCard(array $data, $user_id=null): ?array
+    {
+        
+        $subActions = app(SubscriptionUserActions::class);
+
+        return $subActions->storeCard($data);
+
+    }
+
+    public function getUserPrimaryCard( $user_id ): ?array
+    {
+        
+        $userCardRepo = app(UserPaymentCardRepo::class);
+
+        $primaryCard = $userCardRepo->getUserPrimaryCard($user_id);
+
+        if( $primaryCard ) {
+            return [
+                'customerProfileId' => $primaryCard['Meta']['authnet_profile_id'],
+                'paymentProfileId' => $primaryCard['Meta']['authnet_payment_profile_id'],
+            ];
+        } else {
+            return null;
+        }
+        
+
+    }
+
+}
+
