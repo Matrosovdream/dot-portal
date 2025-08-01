66fc12321f60f6789a6e7afef846cd7b8b69a17d
Stan Matrosov <matrosovdream@gmail.com>
2025-08-01 16:15:47 +0200

    SubscriptionSeeder, new fields added

--- database/seeders/SubscriptionSeeder.php
+++ database/seeders/SubscriptionSeeder.php
@@ -16,62 +16,50 @@ class SubscriptionSeeder extends Seeder
         
         $subs = [
             [
-                'name' => '5 Drivers', 
-                'price' => 150.00,
+                'name' => '1-5 Drivers ($30 per driver)', 
+                'slug' => '1-5-drivers',
+                'price_per_driver' => 30.00,
+                'drivers_amount_from' => 1,
+                'drivers_amount_to' => 5,
                 'discount' => 0,
                 'duration' => 'monthly',
                 'short_description' => 'Best for a small company',
-                'description' => '1-5 drivers for 150$ per month',
-                'drivers_amount' => 5,
+                'description' => '1-5 drivers for $30 per driver',
                 'points' => [
-                    [
-                        'title' => 'Up to 5 Active Drivers',
-                        'included' => 1,
-                    ],
-                    [
-                        'title' => 'Request section for free',
-                        'included' => 1,
-                    ],
-                ],
+                    [ 'title' => 'Up to 5 Active Drivers', 'included' => 1 ],
+                    [ 'title' => 'Request section for free', 'included' => 1 ]
+                ]
             ],
             [
-                'name' => '10 Drivers', 
-                'price' => 250.00,
+                'name' => '6-10 Drivers ($25 per driver)',
+                'slug' => '6-10-drivers',
+                'price_per_driver' => 25.00,
+                'drivers_amount_from' => 6,
+                'drivers_amount_to' => 10,
                 'discount' => 0,
                 'duration' => 'monthly',
-                'short_description' => 'Best for a middle company',
-                'description' => '6-10 drivers for 99.99$ per month',
-                'drivers_amount' => 10,
+                'short_description' => 'Best for a medium company',
+                'description' => '6-10 drivers for $25 per driver',
                 'points' => [
-                    [
-                        'title' => 'Up to 10 Active Drivers',
-                        'included' => 1,
-                    ],
-                    [
-                        'title' => 'Request section for free',
-                        'included' => 1,
-                    ],
-                ],
+                    [ 'title' => 'Up to 10 Active Drivers', 'included' => 1 ],
+                    [ 'title' => 'Request section for free', 'included' => 1 ]
+                ]
             ],
             [
-                'name' => '20 Drivers', 
-                'price' => 300.00,
+                'name' => '11-100 Drivers ($20 per driver)',
+                'slug' => '11-100-drivers',
+                'price_per_driver' => 20.00,
+                'drivers_amount_from' => 11,
+                'drivers_amount_to' => 100, 
                 'discount' => 0,
                 'duration' => 'monthly',
-                'short_description' => 'Best for a middle+ company',
-                'description' => '11+ drivers for 300$ per month',
-                'drivers_amount' => 20,
+                'short_description' => 'Best for a large company',
+                'description' => '11+ drivers for $20 per driver',
                 'points' => [
-                    [
-                        'title' => '11+ Active Drivers',
-                        'included' => 1,
-                    ],
-                    [
-                        'title' => 'Request section for free',
-                        'included' => 1,
-                    ],
-                ],
-            ],
+                    [ 'title' => 'Unlimited Active Drivers', 'included' => 1 ],
+                    [ 'title' => 'Request section for free', 'included' => 1 ]
+                ]
+            ]   
         ];
         
         foreach ($subs as $item) {
@@ -79,7 +67,12 @@ class SubscriptionSeeder extends Seeder
             $points = $item['points'];
             unset($item['points']);
 
-            $sub = Subscription::firstOrCreate($item);
+            $sub = Subscription::updateOrCreate(
+                [
+                    'slug' => $item['slug']
+                ],
+                $item
+            );
 
             // Add points
             foreach ($points as $point) {
