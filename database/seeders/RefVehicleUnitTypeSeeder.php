6208e0a3e819c4493aed8ca7e6aab000a16ad765
Stan Matrosov <matrosovdream@gmail.com>
2025-08-08 20:30:20 +0200

    RefVehicleUnitTypeSeeder, changed the names of items

--- database/seeders/RefVehicleUnitTypeSeeder.php
+++ database/seeders/RefVehicleUnitTypeSeeder.php
@@ -11,10 +11,11 @@ class RefVehicleUnitTypeSeeder extends Seeder
 
     public function run()
     {
+
         $roles = [
-            ['name' => 'Big', 'code' => 'big'],
-            ['name' => 'Medium', 'code' => 'medium'],
-            ['name' => 'Small', 'code' => 'small'],
+            ['name' => 'Under 10,001 lbs', 'code' => 'big'],
+            ['name' => '10,001–26,000 lbs', 'code' => 'medium'],
+            ['name' => '26,001 lbs or more', 'code' => 'small'],
         ];
 
         foreach ($roles as $role) {
