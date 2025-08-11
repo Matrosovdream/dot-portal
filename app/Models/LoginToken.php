c09f4c4e27f171b7fe1e803cc5fbfdcc0e4b208f
Stan Matrosov <matrosovdream@gmail.com>
2025-08-11 17:55:32 +0200

    LoginToken model, initial version

---
+++ app/Models/LoginToken.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LoginToken extends Model
+{
+    protected $fillable = [
+        'user_id', 
+        'token', 
+        'expires_at'
+    ];
+    protected $dates = ['expires_at'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
