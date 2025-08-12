d451480c02017c7150f1f44af92e00affe77eaaa
Stan Matrosov <matrosovdream@gmail.com>
2025-08-12 18:33:02 +0200

    SubManagerController, initial version

--- app/Http/Controllers/Dashboard/SubManagerController.php
+++ app/Http/Controllers/Dashboard/SubManagerController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Actions\Dashboard\SubRequestsActions;
+use App\Actions\Dashboard\SubManagerActions;
 
 class SubManagerController extends Controller
 {
