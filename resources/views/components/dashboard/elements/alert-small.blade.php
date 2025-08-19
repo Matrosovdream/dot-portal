cb1c6be0e80a501bc547b8a78b9c7546b0675776
Stan Matrosov <matrosovdream@gmail.com>
2025-08-19 19:25:50 +0200

    Component AlertSmall, initial version

---
+++ resources/views/components/dashboard/elements/alert-small.blade.php
@@ -0,0 +1,5 @@
+<i class="ki-duotone ki-information-5 text-danger fs-2x {{ $attributes->get('class') }}">
+    <span class="path1"></span>
+    <span class="path2"></span>
+    <span class="path3"></span>
+</i>
