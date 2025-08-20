6c9607fe8e421255d3bb0a1fc91d847aa827e850
Stan Matrosov <matrosovdream@gmail.com>
2025-08-20 17:05:22 +0200

    UserObserver, some cleaning up

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,16 +9,13 @@ use App\Repositories\User\UserSubscriptionRepo;
 class UserObserver
 {
 
-    protected $userSubRepo;
-
-    public function __construct()
+    public function __construct(
+        protected UserSubscriptionRepo $userSubRepo
+    )
     {
-        $this->userSubRepo = new UserSubscriptionRepo();
+
     }
 
-    /**
-     * Handle the User "created" event.
-     */
     public function created(User $user): void
     {
 
@@ -32,33 +29,21 @@ class UserObserver
 
     }
 
-    /**
-     * Handle the User "updated" event.
-     */
     public function updated(User $user): void
     {
-        //
+
     }
 
-    /**
-     * Handle the User "deleted" event.
-     */
     public function deleted(User $user): void
     {
         //
     }
 
-    /**
-     * Handle the User "restored" event.
-     */
     public function restored(User $user): void
     {
         //
     }
 
-    /**
-     * Handle the User "force deleted" event.
-     */
     public function forceDeleted(User $user): void
     {
         //
