c1507a62efb90f02f83cb602a84860ffc313302e
Stan Matrosov <matrosovdream@gmail.com>
2025-08-20 17:03:02 +0200

    DriverObserver, injected DriverValidation class

---
+++ app/Observers/Driver/DriverObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers\Driver;
+
+use App\Models\Driver;
+use App\Helpers\Driver\DriverValidationHelper;
+use App\Helpers\Validation\Models\DriverValidation;
+
+
+
+class DriverObserver
+{
+
+    public function __construct(
+        //protected DriverValidationHelper $validationHelper
+        protected DriverValidation $driverValidation 
+    )
+    { }
+
+    public function created(Driver $driver): void
+    {
+        // Validate driver data and update events if necessary
+        $this->driverValidation->updateUserTasks($driver->id);
+    }
+
+    public function updated(Driver $driver): void
+    {
+
+        // Validate driver data and update events if necessary
+        $this->driverValidation->updateUserTasks($driver->id);
+
+    }
+
+    public function deleted(Driver $driver): void
+    {
+        // Validate driver data and update events if necessary
+        $this->driverValidation->updateUserTasks($driver->id);
+    }
+
+    public function restored(Driver $driver): void
+    {
+
+
+    }
+
+    public function forceDeleted(Driver $driver): void
+    {
+        //
+    }
+
+}
