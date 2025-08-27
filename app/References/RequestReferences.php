1b28aec7edb87882e8ea674b91fa0278904fc2f4
Stan Matrosov <matrosovdream@gmail.com>
2025-08-27 20:36:24 +0200

    RequestReferences, new set of statuses

--- app/References/RequestReferences.php
+++ app/References/RequestReferences.php
@@ -6,13 +6,11 @@ class RequestReferences {
     public function getRequestStatuses() {
 
         return [
-            ['name' => 'Processing', 'slug' => 'processing', 'color' => 'blue', 'published' => 1],
-            ['name' => 'Waiting for payment', 'slug' => 'waiting-payment', 'color' => 'blue', 'published' => 1],
-            ['name' => 'On hold', 'slug' => 'hold', 'color' => 'gray', 'published' => 1],
-            ['name' => 'Pending verification', 'slug' => 'pending', 'color' => 'gray', 'published' => 1],
-            ['name' => 'Submitted', 'slug' => 'submitted', 'color' => 'gray', 'published' => 1],
-            ['name' => 'Completed', 'slug' => 'completed', 'color' => 'green', 'published' => 1],
-            ['name' => 'Cancelled', 'slug' => 'cancelled', 'color' => 'red', 'published' => 1],
+            ['name' => 'Pending', 'slug' => 'pending', 'color' => 'blue', 'published' => 1],
+            ['name' => 'Consent Not Given', 'slug' => 'consent-not-given', 'color' => 'gray', 'published' => 1],
+            ['name' => 'Accepted / Completed', 'slug' => 'accepted-completed', 'color' => 'green', 'published' => 1],
+            ['name' => 'Passed / No Record', 'slug' => 'passed-no-record', 'color' => 'green', 'published' => 1],
+            ['name' => 'Violation Found', 'slug' => 'violation-found', 'color' => 'red', 'published' => 1],
         ];
         
     }
