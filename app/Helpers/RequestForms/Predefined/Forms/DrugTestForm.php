80d7e5b32d6137605e1ffe09bed6d7a34b39aebd
Stan Matrosov <matrosovdream@gmail.com>
2025-08-29 15:35:12 +0200

    Predefined form Drug Test, initial version

---
+++ app/Helpers/RequestForms/Predefined/Forms/DrugTestForm.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Helpers\RequestForms\Predefined\Forms;
+
+use App\Helpers\RequestForms\Predefined\AbstractForm;
+
+class DrugTestForm extends AbstractForm
+{
+    protected $formName = 'ifta';
+    protected $formTitle = 'IFTA';
+    protected $formFields = [
+        'request_type' => [
+            'type' => 'select',
+            'label' => 'Request Type',
+            'reference' => 'request_type',
+            'required' => true,
+        ],
+        'country_state_id' => [
+            'type' => 'select',
+            'label' => 'State',
+            'reference' => 'country_state',
+            'required' => true,
+        ],
+        'vehicle_id' => [
+            'type' => 'select',
+            'label' => 'Vehicle',
+            'reference' => 'vehicles',
+            'required' => true,
+        ],
+        'filing_period' => [
+            'type' => 'select',
+            'label' => 'Filing Period',
+            'reference' => 'filing_period',
+        ],
+    ];
+
+
+    public function validateFormData($requestData)
+    {
+
+        $errors = [];
+
+        if( $requestData['request_type'] == 'reg' ) {
+            if( empty($requestData['country_state_id']) ) {
+                $errors['country_state'] = 'State is required field.';
+            }
+            if( empty($requestData['vehicle_id']) ) {
+                $errors['vehicle'] = 'Vehicle is required field.';
+            }
+        } elseif( $requestData['request_type'] == 'quarterly_filing' ) {
+            if( empty($requestData['filing_period']) ) {
+                $errors['filing_period'] = 'Period is required field.';
+            }
+            if( empty($requestData['vehicle_id']) ) {
+                $errors['vehicle'] = 'Vehicle is required field.';
+            }
+        }
+
+        return $errors;
+    }
+
+    public function getReferences() {
+
+        $fields = [
+            'country_state' => [
+                'type' => 'select',
+                'label' => 'State',
+                'options' => $this->getCountryStates(),
+            ],
+            'vehicles' => [
+                'type' => 'select',
+                'label' => 'Vehicles',
+                'options' => $this->getVehicles(
+                    ['company_id' => auth()->user()->company->id ?? null]
+                ), 
+            ],
+            'filing_period' => [
+                'type' => 'select',
+                'label' => 'Filing Period',
+                'options' => $this->getQuarterPeriods(),
+            ],
+        ];
+
+        return $this->prepareReferences($fields);
+
+    }
+
+
+}
---
+++ resources/views/forms/request/drug-test.blade.php
@@ -0,0 +1,96 @@
+<!-- Change Type -->
+<div class="row mb-6">
+
+  <label class="col-lg-4 col-form-label fw-semibold fs-6">Change Type</label>
+  <div class="col-lg-4 fv-row">
+    <select name="fields[request_type]" id="requestType" class="form-select form-select-lg form-select-solid">
+      <option value="reg" @if( $values['request_type'] == 'reg' ) selected @endif>
+        Registration
+      </option>
+      <option value="quarterly_filing" @if( $values['request_type'] == 'quarterly_filing' ) selected @endif>
+        Quarterly Filing
+      </option>
+    </select>
+  </div>
+
+</div>
+
+<div id="tab_registration">
+  
+  <div class="row mb-6">
+    <x-select 
+        inputName="fields[country_state_id]"
+        label="State"
+        :options="$formRefs['country_state']['options']"
+        value="{{ $values['country_state_id'] ?? '' }}"
+        :multiple=false
+        :required=true
+        template="inline"
+    />
+  </div>
+
+  <div class="row mb-6">
+    <x-select 
+      inputName="fields[vehicle_id]"
+      label="Choose Vehicle"
+      inputId="vehicles1"
+      :options="$formRefs['vehicles']['options']"
+      value="{{ $values['vehicle_id'] ?? '' }}"
+      :multiple=false
+      :required=true
+      template="inline"
+    />
+  </div>
+
+</div>  
+
+<div id="tab_quarterly_filing">
+
+  <div class="row mb-6">
+    <x-select 
+        inputName="fields[filing_period]"
+        label="Period"
+        :options="$formRefs['filing_period']['options']"
+        value="{{ $values['filing_period'] ?? '' }}"
+        :multiple=false
+        :required=true
+        template="inline"
+    />
+  </div>
+
+  <div class="row mb-6">
+    <x-select 
+      inputName="fields[vehicle_id]"
+      inputId="vehicles2"
+      label="Choose Vehicle"
+      :options="$formRefs['vehicles']['options']"
+      value="{{ $values['vehicle_id'] ?? '' }}"
+      :multiple=false
+      :required=true
+      template="inline"
+    />
+  </div>
+
+</div>
+
+
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    const requestType = document.getElementById('requestType');
+    const tabRegistration = document.getElementById('tab_registration');
+    const tabQuarterlyFiling = document.getElementById('tab_quarterly_filing');
+
+    function toggleTabs() {
+      if (requestType.value === 'reg') {
+        tabRegistration.style.display = 'block';
+        tabQuarterlyFiling.style.display = 'none';
+      } else {
+        tabRegistration.style.display = 'none';
+        tabQuarterlyFiling.style.display = 'block';
+      }
+    }
+
+    requestType.addEventListener('change', toggleTabs);
+    toggleTabs(); // Initial call to set the correct tab on page load
+  });
+</script>
