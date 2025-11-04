9bd08694f49b0200fddcd7b37399ca586a1279fc
Stan Matrosov <matrosovdream@gmail.com>
2025-11-04 22:34:35 +0100

    UserQueryBalanceHistoryRepo, initial version

---
+++ app/Repositories/User/UserQueryBalanceHistoryRepo.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Repositories\User;
+
+use App\Repositories\AbstractRepo;
+use App\Models\UserQueryBalanceHistory;
+
+
+class UserQueryBalanceHistoryRepo extends AbstractRepo
+{
+
+    public $model;
+
+    protected $fields = [];
+
+    protected $withRelations = [];
+
+    protected $userRepo;
+    protected $metaRepo;
+
+    public function __construct()
+    {
+        $this->model = new UserQueryBalanceHistory();
+    }
+
+    public function mapItem($item)
+    {
+
+        if( empty($item) ) {
+            return null;
+        }
+
+        $res = [
+            'id' => $item->id,
+            'user_id' => $item->user_id,
+            'user_company_id' => $item->user_company_id,
+            'amount' => $item->amount,
+            'type' => $item->type,
+            'initiator' => $item->initiator,
+            'initiator_id' => $item->initiator_id,
+            'notes' => $item->notes,
+            'Model' => $item
+        ];
+
+        return $res;
+    }
+
+}
