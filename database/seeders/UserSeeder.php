91a220d83f7f787bac3b3698d25a456231e16862
Stan Matrosov <matrosovdream@gmail.com>
2026-06-08 23:00:49 +0700

    UserSeeder, removed extra company users but one simple left

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,7 +1,6 @@
 <?php
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
@@ -14,7 +13,7 @@ class UserSeeder extends Seeder
         $users = [
             ['firstname' => 'Admin', 'email' => 'admin@gmail.com', 'password' => '123456', 'role' => 1],
             ['firstname' => 'Manager', 'email' => 'manager@gmail.com', 'password' => '123456', 'role' => 2],
-            ['firstname' => 'Company dot #18235', 'email' => 'company18235@gmail.com', 'password' => '123456', 'role' => 3,
+            ['firstname' => 'Company', 'email' => 'company@gmail.com', 'password' => '123456', 'role' => 3,
                 'company' => [
                     'name' => 'Company dot #18235',
                     'phone' => '+381123456789',
@@ -22,36 +21,7 @@ class UserSeeder extends Seeder
                     'mc_number' => '',
                 ]
             ],
-            ['firstname' => 'Company dot #44', 'email' => 'company44@gmail.com', 'password' => '123456', 'role' => 3,
-                'company' => [
-                    'name' => 'Company dot #44',
-                    'phone' => '+381123456789',
-                    'dot_number' => '44',
-                    'mc_number' => '',
-                ]
-            ],
-            ['firstname' => 'Company dot #363', 'email' => 'company363@gmail.com', 'password' => '123456', 'role' => 3,
-                'company' => [
-                    'name' => 'Company dot #363',
-                    'phone' => '+381123456789',
-                    'dot_number' => '363',
-                    'mc_number' => '',
-                ]
-            ],
             ['firstname' => 'Driver', 'email' => 'driver@gmail.com', 'password' => '123456', 'role' => 4],
-        ];
-
-        $companies = $this->generateCompanies([6500, 6550]);
-        //$users = array_merge($users, $companies);
-
-        // user and company with dot 109163, for delay testing
-        $users[] = ['firstname' => 'Company dot #109163', 'email' => 'company363@gmail.com', 'password' => '123456', 'role' => 3,
-            'company' => [
-                'name' => 'Company dot #109163',
-                'phone' => '+381123456789',
-                'dot_number' => '109163',
-                'mc_number' => '',
-            ]
         ];
 
         foreach ($users as $userData) {
@@ -79,30 +49,6 @@ class UserSeeder extends Seeder
             // Assign roles
             $user->roles()->sync($userData['role']);
         }
-
-    }
-
-    private function generateCompanies( $range=[6500, 6600] ) {
-
-        $users = [];
-        for ($i = $range[0]; $i < $range[1]; $i++) {
-
-            $users[] = [
-                'firstname' => 'Company dot #' . $i,
-                'email' => 'company' . $i . '@gmail.com',
-                'password' => '123456',
-                'role' => 3, // Assuming role 3 is for companies
-                'company' => [
-                    'name' => 'Company dot #' . $i,
-                    'phone' => '+3811'.$i,
-                    'dot_number' => $i,
-                    'mc_number' => '',
-                ]
-            ];
-
-        }
-
-        return $users;
 
     }
 
